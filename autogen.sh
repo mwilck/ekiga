e5bba511d11fbb945efe6635cabdd57d473bae55
Christopher R. Gabriel <cgabriel@src.gnome.org>
2001-09-06 15:33:58 +0000

    Oh, well.
    Removing macros and intl from this module and then adding them to
    CVSROOT/module won't work, they're pulled out of Attic. So? re-importing all
    this stuff, and we'll see.

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,44 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-
 srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+PROJECT="gnomemeeting"
+TEST_TYPE=-d
 
-PKG_NAME="gnomemeeting"
 
-(test -f $srcdir/configure.in \
-  && test -d $srcdir/src \
-  && test -f $srcdir/src/main.h) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level gnomemeeting directory"
-    exit 1
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	DIE=1
 }
 
-. $srcdir/macros/autogen.sh
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Get ftp://ftp.cygnus.com/pub/home/tromey/automake-1.2d.tar.gz"
+	echo "(or a newer version if it is available)"
+	DIE=1
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+case $CC in
+*lcc | *lcc\ *) am_opt=--include-deps;;
+esac
+
+echo "Running autoconf"
+aclocal  -I ./macros $ACLOCAL_FLAGS
+
+echo "Running automake"
+automake -a --gnu $am_opt
+autoconf
+
+echo "Running configure"
+$srcdir/configure "$@"
 
