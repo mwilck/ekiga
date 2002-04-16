a5afe38c5328f946b817f074984b6df32cd08eed
Kenneth Rohde Christiansen <kenneth@gnu.org>
2002-04-16 23:43:04 +0000

    Ported away from GNOMEUIINFO and used GtkToolbar instead. Also changed the
    2002-04-17  Kenneth Rohde Christiansen  <kenneth@gnu.org>
    
    	* src/toolbar.cpp (gnomemeeting_init_toolbar): Ported away from
    	GNOMEUIINFO and used GtkToolbar instead. Also changed the icon
    	handling and made the stock icons antialiased. A few is still
    	missing.
    
    	* pixmaps/text-chat.png: New file.
    
    	* pixmaps/ils-directory.png: New file.
    
    	* pixmap/inlines.h: New file, will be generated later.

--- src/toolbar.cpp
+++ src/toolbar.cpp
@@ -37,6 +37,7 @@
 #include "connection.h"
 #include "common.h"
 #include "misc.h" 
+#include "../pixmaps/inlines.h"
 
 #include "../pixmaps/connect.xpm"
 #include "../pixmaps/disconnect.xpm"
@@ -88,11 +89,11 @@ void connect_button_update_pixmap (GtkToggleButton *w, int pressed)
 
       /* Block the signal */
       g_signal_handlers_block_by_func (G_OBJECT (w), 
-				       (gpointer) connect_button_clicked, 
+				       (void *) connect_button_clicked, 
 				       (gpointer) gw->connect_button);
       gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (w), TRUE);
       g_signal_handlers_unblock_by_func (G_OBJECT (w), 
-				       (gpointer) connect_button_clicked, 
+				       (void *) connect_button_clicked, 
 				       (gpointer) gw->connect_button);
     }
     else {
@@ -100,11 +101,11 @@ void connect_button_update_pixmap (GtkToggleButton *w, int pressed)
       pixbuf = gdk_pixbuf_new_from_xpm_data ((const char **) disconnect_xpm);
   
       g_signal_handlers_block_by_func (G_OBJECT (w), 
-				       (gpointer) connect_button_clicked, 
+				       (void *) connect_button_clicked, 
 				       (gpointer) gw->connect_button);
       gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (w), FALSE);
       g_signal_handlers_unblock_by_func (G_OBJECT (w), 
-					 (gpointer) connect_button_clicked, 
+					 (void *) connect_button_clicked, 
 					 (gpointer) gw->connect_button);
     }
 
@@ -148,44 +149,46 @@ void gnomemeeting_init_toolbar ()
   GtkWidget *pixmap;
   GdkPixmap *Pixmap;
   GdkBitmap *mask;
-  GdkPixbuf *pixbuf;
+  GdkPixbuf *pixbuf, *ils_directory, *text_chat;
   GtkWidget *image;
   GtkWidget *hbox;
+  GtkWidget *left_toolbar;
 
   GtkTooltips *tip;
   GConfClient *client = gconf_client_get_default ();
 
   GM_window_widgets *gw = gnomemeeting_get_main_window (gm);
-  
-  static GnomeUIInfo left_toolbar [] =
-    {
-      {
-	GNOME_APP_UI_ITEM,
-	N_("ILS Directory"), N_("Find friends on ILS"),
-	(void *) gnomemeeting_component_view, gw->ldap_window, NULL,
-	GNOME_APP_PIXMAP_DATA, xdap_directory_xpm,
-	NULL, GDK_CONTROL_MASK, NULL
-	},
-        {
-	GNOME_APP_UI_ITEM,
-	N_("Chat"), N_("Make a text chat with your friend"),
-	(void *) toolbar_toggle_changed, 
-	(gpointer) "/apps/gnomemeeting/view/show_chat_window",
-	NULL, GNOME_APP_PIXMAP_DATA, text_chat_xpm,
-	NULL, GDK_CONTROL_MASK, NULL
-	},
-	{
-	GNOME_APP_UI_ITEM,
-	N_("Control Panel"), N_("Display the control panel"),
-	(void *) toolbar_toggle_changed, 
-	(gpointer) "/apps/gnomemeeting/view/show_control_panel",
-	NULL, GNOME_APP_PIXMAP_DATA, settings_xpm,
-	NULL, GDK_CONTROL_MASK, NULL
-	},
-	GNOMEUIINFO_SEPARATOR,
-	GNOMEUIINFO_END
-    };
 
+  left_toolbar = gtk_toolbar_new ();
+	  
+  ils_directory = gdk_pixbuf_new_from_inline (-1, inline_ils_directory, FALSE, NULL);
+  text_chat     = gdk_pixbuf_new_from_inline (-1, inline_text_chat, FALSE, NULL);
+
+  image = gtk_image_new_from_stock (GTK_STOCK_PREFERENCES, GTK_ICON_SIZE_LARGE_TOOLBAR);
+  
+  gtk_toolbar_append_item  (GTK_TOOLBAR (left_toolbar),
+                            N_("ILS Directory"),
+                            N_("Find friends on ILS"),
+                            NULL,
+                            gtk_image_new_from_pixbuf (ils_directory),
+                            GTK_SIGNAL_FUNC (gnomemeeting_component_view),
+                            (gpointer) gw->ldap_window); 
+
+  gtk_toolbar_append_item  (GTK_TOOLBAR (left_toolbar),
+                            N_("Chat"),
+                            N_("Make a text chat with your friend"),                           
+                            NULL,                              
+                            gtk_image_new_from_pixbuf (text_chat),    
+                            GTK_SIGNAL_FUNC (toolbar_toggle_changed),
+                            (gpointer) "/apps/gnomemeeting/view/show_chat_window");
+  
+  gtk_toolbar_append_item  (GTK_TOOLBAR (left_toolbar),
+                            N_("Control Panel"),
+                            N_("Display the control panel"),
+                            NULL,
+                            image,
+                            GTK_SIGNAL_FUNC (toolbar_toggle_changed),
+                            (gpointer) "/apps/gnomemeeting/view/show_control_panel");
 
   /* Both toolbars */
 
@@ -194,8 +197,8 @@ void gnomemeeting_init_toolbar ()
 
 
   /* Combo */
-  gw->combo = gnomemeeting_history_combo_box_new("/apps/gnomemeeting/"
- 						 "history/called_hosts");
+  gw->combo = gnomemeeting_history_combo_box_new ("/apps/gnomemeeting/"
+ 						  "history/called_hosts");
 
   gtk_combo_set_use_arrows_always (GTK_COMBO(gw->combo), TRUE);
 
@@ -234,16 +237,12 @@ void gnomemeeting_init_toolbar ()
   			BONOBO_DOCK_ITEM_BEH_EXCLUSIVE,
   			BONOBO_DOCK_TOP, 1, 0, 0);
 
-
-  /* The vertical toolbar on the left */
-  GtkWidget *toolbar2 = gtk_toolbar_new ();
-
-  gnome_app_fill_toolbar(GTK_TOOLBAR (toolbar2), left_toolbar, NULL);
-  gnome_app_add_toolbar (GNOME_APP (gm), GTK_TOOLBAR (toolbar2),
+  gnome_app_add_toolbar (GNOME_APP (gm), GTK_TOOLBAR (left_toolbar),
  			 "left_toolbar", BONOBO_DOCK_ITEM_BEH_EXCLUSIVE,
  			 BONOBO_DOCK_LEFT, 3, 0, 0);
-  gtk_toolbar_set_style (GTK_TOOLBAR (toolbar2), GTK_TOOLBAR_ICONS);
 
+  gtk_toolbar_set_style (GTK_TOOLBAR (left_toolbar), GTK_TOOLBAR_ICONS);
+ 
   /* Video Preview Button */
   gw->preview_button = gtk_toggle_button_new ();
 
@@ -253,7 +252,8 @@ void gnomemeeting_init_toolbar ()
   gtk_container_add (GTK_CONTAINER (gw->preview_button), GTK_WIDGET (image));
   g_object_unref (pixbuf);
 
-  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (gw->preview_button), gconf_client_get_bool (client, "/apps/gnomemeeting/devices/video_preview", NULL));
+  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (gw->preview_button), 
+				gconf_client_get_bool (client, "/apps/gnomemeeting/devices/video_preview", NULL));
 
   g_signal_connect (G_OBJECT (gw->preview_button), "clicked",
 		    G_CALLBACK (toolbar_toggle_changed), 
@@ -263,7 +263,7 @@ void gnomemeeting_init_toolbar ()
   gtk_tooltips_set_tip (tip, gw->preview_button,
 			_("Click here to begin to display images from your camera device."), NULL);
 
-  gtk_toolbar_append_widget (GTK_TOOLBAR (toolbar2), 
+  gtk_toolbar_append_widget (GTK_TOOLBAR (left_toolbar), 
 			     gw->preview_button, NULL, NULL);
 
 
@@ -286,22 +286,21 @@ void gnomemeeting_init_toolbar ()
   gtk_tooltips_set_tip (tip, gw->audio_chan_button,
 			_("Audio Transmission Status. During a call, click here to pause the audio transmission."), NULL);
 
-  gtk_toolbar_append_widget (GTK_TOOLBAR (toolbar2), 
+  gtk_toolbar_append_widget (GTK_TOOLBAR (left_toolbar), 
 			     gw->audio_chan_button, NULL, NULL);
 
 
   /* Video Channel Button */
   gw->video_chan_button = gtk_toggle_button_new ();
 
-  pixbuf = 
-    gdk_pixbuf_new_from_xpm_data ((const char **) quickcam_xpm);
+  pixbuf = gdk_pixbuf_new_from_xpm_data ((const char **) quickcam_xpm);
   image = gtk_image_new_from_pixbuf (pixbuf);
   gtk_container_add (GTK_CONTAINER (gw->video_chan_button), 
 		     GTK_WIDGET (image));
   g_object_unref (pixbuf);
-
+/*
   gtk_widget_set_sensitive (GTK_WIDGET (gw->video_chan_button), FALSE);
-
+*/
   g_signal_connect (G_OBJECT (gw->video_chan_button), "clicked",
 		    G_CALLBACK (pause_video_callback), gw);
 
@@ -309,7 +308,7 @@ void gnomemeeting_init_toolbar ()
   gtk_tooltips_set_tip (tip, gw->video_chan_button,
 			_("Video Transmission Status. During a call, click here to pause the video transmission."), NULL);
 
-  gtk_toolbar_append_widget (GTK_TOOLBAR (toolbar2), 
+  gtk_toolbar_append_widget (GTK_TOOLBAR (left_toolbar), 
 			     gw->video_chan_button, NULL, NULL);
 
 
