7b8619233f9e042f17584818b307167769619c91
Kenneth Rohde Christiansen <kenneth@gnu.org>
2002-05-05 22:04:35 +0000

    Added some emoticons from gtkhtml.
    2002-05-05  Kenneth Rohde Christiansen  <kenneth@gnu.org>
    
    	* pixmaps/inline_emoticons.h: Added some emoticons from gtkhtml.
    
    	* src/chat_window.cpp (gtk_text_buffer_insert_with_emoticons):
    	New function to insert text in a text buffer, with emoticons
    	shows as pictures.
    	(gnomemeeting_chat_window_text_insert): Changes to use the above

--- src/chat_window.cpp
+++ src/chat_window.cpp
@@ -1,6 +1,7 @@
-
-/* GnomeMeeting -- A Video-Conferencing application
- * Copyright (C) 2000-2001 Damien Sandras
+/*
+ * GnomeMeeting -- A Video-Conferencing application
+ *
+ * Copyright (C) 2000-2002 Damien Sandras
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +16,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Author(s) of this file: Damien Sandras <dsandras@seconix.com>
+ *                         Kenneth Christiansen <kenneth@gnu.org>
+ * 
  */
 
 /*
@@ -41,174 +46,353 @@
 #include "gnomemeeting.h"
 #include "misc.h"
 #include "common.h"
-
+#include "../pixmaps/inline_emoticons.h"
 
 extern GtkWidget *gm;
 extern GnomeMeeting *MyApp;
 
+void
+gtk_text_buffer_insert_with_emoticons (GtkTextBuffer *buf, 
+                                       GtkTextIter *bufiter, 
+                                       const char *text)
+{
+    typedef enum {
+        GM_EMOTICON_STATE_NO_SMILEY,
+        GM_EMOTICON_EYES_NORMAL,
+        GM_EMOTICON_EYES_ONE_CLOSED,
+        GM_EMOTICON_HUGE_EYES,
+        
+        /* Noses and tears are ignored */
+        
+        GM_EMOTICON_STATE_BIG_SMILE,  /* ;D  */
+        GM_EMOTICON_STATE_SHOCKED,    /* ;O  */
+        GM_EMOTICON_STATE_SMILING,    /* :)  */
+        GM_EMOTICON_STATE_EYE_BLINK,  /* ;)  */
+        GM_EMOTICON_STATE_BIG_EYES,   /* 8)  */
+        GM_EMOTICON_STATE_LOOKING_SAD /* :(  */
+    } GmEmoticonState;
+    
+    GdkPixbuf *emoticon;
+    GmEmoticonState state;
+    
+    char *iter;
+    char *str;
+    char *orig_str;
+    char *emoticon_start;
+    char *emoticon_end;
+    gboolean end = FALSE;
+
+    str = g_strdup_printf ("%s", text);
+    orig_str = str;
+    
+    for (iter = str, state = GM_EMOTICON_STATE_NO_SMILEY;
+         end == FALSE; iter++)
+    {
+        switch (state)
+        {
+        case GM_EMOTICON_EYES_NORMAL:
+        case GM_EMOTICON_EYES_ONE_CLOSED:
+        case GM_EMOTICON_HUGE_EYES:
+            switch (*iter)
+            {
+            case '-':
+                /* ignoring noses and tears*/
+                break;
+                
+            case 'D':
+                emoticon_end = iter + 1;
+                state = GM_EMOTICON_STATE_BIG_SMILE;
+                break;
+                
+            case '0': case 'O':
+                emoticon_end = iter + 1;
+                state = GM_EMOTICON_STATE_SHOCKED;
+                break;
+                
+            case '(':
+                emoticon_end = iter + 1;
+                state = GM_EMOTICON_STATE_LOOKING_SAD;
+                break;
+                
+            case ')':
+                switch (state)
+                {
+                case GM_EMOTICON_EYES_NORMAL:
+                    emoticon_end = iter + 1;
+                    state = GM_EMOTICON_STATE_SMILING;
+                    break;
+                    
+                case GM_EMOTICON_EYES_ONE_CLOSED:
+                    emoticon_end = iter + 1;
+                    state = GM_EMOTICON_STATE_EYE_BLINK;
+                    break;
+                                        
+                case GM_EMOTICON_HUGE_EYES:
+                    emoticon_end = iter + 1;
+                    state = GM_EMOTICON_STATE_BIG_EYES;
+                    break;
+                }
+                break;
+                
+            default:
+                state = GM_EMOTICON_STATE_NO_SMILEY;
+                break;
+            }
+            break;
+            
+        case GM_EMOTICON_STATE_NO_SMILEY:
+            switch (*iter)
+            {
+                
+            case ';':
+                emoticon_start = iter;
+                state = GM_EMOTICON_EYES_ONE_CLOSED;
+                break;
+                
+            case ':':
+                emoticon_start = iter;
+                state = GM_EMOTICON_EYES_NORMAL;
+                break;
+                
+            case '8':
+                emoticon_start = iter;
+                state = GM_EMOTICON_HUGE_EYES;
+                break;       
+                
+            default:
+                break;        
+            }
+            break;
+            
+        case GM_EMOTICON_STATE_BIG_SMILE:   /* ;D  */
+            *emoticon_start = '\0';
+            
+            gtk_text_buffer_insert (buf, bufiter, str, -1);
+            emoticon = gdk_pixbuf_new_from_inline (-1, inline_smiley_one, FALSE, NULL);
+            gtk_text_buffer_insert_pixbuf (buf, bufiter, emoticon);
+            
+            str = emoticon_end;
+            state = GM_EMOTICON_STATE_NO_SMILEY;
+            break;
+            
+        case GM_EMOTICON_STATE_SHOCKED:     /* ;O  */
+            *emoticon_start = '\0';
+            
+            gtk_text_buffer_insert (buf, bufiter, str, -1);
+            emoticon = gdk_pixbuf_new_from_inline (-1, inline_smiley_two, FALSE, NULL);
+            gtk_text_buffer_insert_pixbuf (buf, bufiter, emoticon);
+            
+            str = emoticon_end;
+            state = GM_EMOTICON_STATE_NO_SMILEY;
+            break;
+            
+        case GM_EMOTICON_STATE_SMILING:     /* :)  */
+            *emoticon_start = '\0';
+            
+            gtk_text_buffer_insert (buf, bufiter, str, -1);
+            emoticon = gdk_pixbuf_new_from_inline (-1, inline_smiley_three, FALSE, NULL);
+            gtk_text_buffer_insert_pixbuf (buf, bufiter, emoticon);
+            
+            str = emoticon_end;
+            state = GM_EMOTICON_STATE_NO_SMILEY;
+            break;
+            
+        case GM_EMOTICON_STATE_EYE_BLINK:   /* ;)  */
+            *emoticon_start = '\0';
+                        
+            gtk_text_buffer_insert (buf, bufiter, str, -1);
+            emoticon = gdk_pixbuf_new_from_inline (-1, inline_smiley_four, FALSE, NULL);
+            gtk_text_buffer_insert_pixbuf (buf, bufiter, emoticon);
+            
+            str = emoticon_end;
+            state = GM_EMOTICON_STATE_NO_SMILEY;
+            break;
+            
+        case GM_EMOTICON_STATE_BIG_EYES:    /* 8)  */
+            *emoticon_start = '\0';
+            
+            gtk_text_buffer_insert (buf, bufiter, str, -1);
+            emoticon = gdk_pixbuf_new_from_inline (-1, inline_smiley_five, FALSE, NULL);
+            gtk_text_buffer_insert_pixbuf (buf, bufiter, emoticon);
+            
+            str = emoticon_end;
+            state = GM_EMOTICON_STATE_NO_SMILEY;
+            break;
+            
+        case GM_EMOTICON_STATE_LOOKING_SAD: /* :(  */
+            *emoticon_start = '\0';
+            
+            gtk_text_buffer_insert (buf, bufiter, str, -1);
+            emoticon = gdk_pixbuf_new_from_inline (-1, inline_smiley_six, FALSE, NULL);
+            gtk_text_buffer_insert_pixbuf (buf, bufiter, emoticon);
+            
+            str = emoticon_end;
+            state = GM_EMOTICON_STATE_NO_SMILEY;
+            break;
+            
+        default:
+            break;
+        }
+    
+        if (*iter == '\0')
+            end = TRUE;
+    }
+
+    gtk_text_buffer_insert (buf, bufiter, str, -1);
+    free (orig_str);
+}
+
 
 static void chat_entry_activate (GtkEditable *w, gpointer data)
 {
-  /* Get the structs from the application */
-  GM_window_widgets *gw = gnomemeeting_get_main_window (gm);
+    /* Get the structs from the application */
+    GM_window_widgets *gw = gnomemeeting_get_main_window (gm);
 
-  GMH323EndPoint *endpoint = MyApp->Endpoint ();
-  PString s;
-
-  // For testing purposes.
-  // s = PString (gtk_entry_get_text (GTK_ENTRY (w)));
-  // gnomemeeting_chat_window_text_insert ("Kenneth", s, 0);
-  // gtk_entry_set_text (GTK_ENTRY (w), "");
-
-  if (endpoint) {
-
-    PString local = endpoint->GetLocalUserName ();
-
-    PINDEX bracket = local.Find('[');
-    if (bracket != P_MAX_INDEX)
-      local = local.Left (bracket);
+    GMH323EndPoint *endpoint = MyApp->Endpoint ();
+    PString s;
     
-    bracket = local.Find('(');
-    if (bracket != P_MAX_INDEX)
-      local = local.Left (bracket);
-    
-    if (endpoint->GetCallingState () == 2) {
+    if (endpoint) {
+        
+        PString local = endpoint->GetLocalUserName ();
+        
+        PINDEX bracket = local.Find('[');
+        if (bracket != P_MAX_INDEX)
+            local = local.Left (bracket);
+        
+        bracket = local.Find('(');
+        if (bracket != P_MAX_INDEX)
+            local = local.Left (bracket);
+        
+        if (endpoint->GetCallingState () == 2) {
+            
+            H323Connection *connection = endpoint->GetCurrentConnection ();
+            
+            if (connection != NULL)  {
+                
+                s = PString (gtk_entry_get_text (GTK_ENTRY (w)));
+                connection->SendUserInput ("MSG"+s);
 
-      H323Connection *connection = endpoint->GetCurrentConnection ();
-
-      if (connection != NULL)  {
-	
-	s = PString (gtk_entry_get_text (GTK_ENTRY (w)));
- 	connection->SendUserInput ("MSG"+s);
-
-	gnomemeeting_chat_window_text_insert (local, s, 0);
-
-	gtk_entry_set_text (GTK_ENTRY (w), "");
-      }
+                gnomemeeting_chat_window_text_insert (local, s, 0);
+                
+                gtk_entry_set_text (GTK_ENTRY (w), "");
+            }
+        }
     }
-  }
 }
 
 
 void gnomemeeting_chat_window_text_insert (PString local, PString str, int user)
 {
-  gchar *msg = NULL;
-  GtkTextIter iter;
-  GtkTextMark *mark;
-  static gboolean first = TRUE;
+    gchar *msg = NULL;
+    GtkTextIter iter;
+    GtkTextMark *mark;
+    static gboolean first = TRUE;
 
-  GM_window_widgets *gw = gnomemeeting_get_main_window (gm);
+    GM_window_widgets *gw = gnomemeeting_get_main_window (gm);
 
-  gtk_text_buffer_get_end_iter (gw->chat_buffer, &iter);
+    gtk_text_buffer_get_end_iter (gw->chat_buffer, &iter);
 
-  if (first) 
-  {
-          msg = g_strdup_printf ("%s: ", (const char *) local);
-          first = FALSE;
-  }
-  else
-  msg = g_strdup_printf ("\n%s: ", (const char *) local);
+    if (first) 
+    {
+        msg = g_strdup_printf ("%s: ", (const char *) local);
+        first = FALSE;
+    }
+    else
+        msg = g_strdup_printf ("\n%s: ", (const char *) local);
 
-  if (user == 1)
-  {
-          gtk_text_buffer_insert_with_tags_by_name (gw->chat_buffer, &iter, 
-                                                    msg, -1,                         
-                                                    "primary-user", NULL);
-  } else {
-          gtk_text_buffer_insert_with_tags_by_name (gw->chat_buffer, &iter, 
-                                                    msg, -1,                         
-                                                    "secondary-user", NULL);
-  }
+    if (user == 1)
+        gtk_text_buffer_insert_with_tags_by_name (gw->chat_buffer, &iter, msg, 
+                                                  -1, "primary-user", NULL);
+    else
+        gtk_text_buffer_insert_with_tags_by_name (gw->chat_buffer, &iter, msg, 
+                                                  -1, "secondary-user", NULL);
   
-  g_free (msg);
+    g_free (msg);
+  
+    gtk_text_buffer_insert_with_emoticons (gw->chat_buffer, &iter, (const char *) str);
 
-  msg = g_strdup_printf ("%s", (const char *) str);
+    mark = gtk_text_buffer_get_mark (gw->chat_buffer, "current-position");
 
-  gtk_text_buffer_insert (gw->chat_buffer, &iter, msg, -1);
-
-  g_free (msg);
-
-  mark = gtk_text_buffer_get_mark (gw->chat_buffer, "current-position");
-
-  gtk_text_view_scroll_to_mark (GTK_TEXT_VIEW (gw->chat_view), mark, 
-                                0.0, FALSE, 0,0);
+    gtk_text_view_scroll_to_mark (GTK_TEXT_VIEW (gw->chat_view), mark, 
+                                  0.0, FALSE, 0,0);
 }
 
 
 void gnomemeeting_init_chat_window ()
 {
-  GtkWidget *entry;
-  GtkWidget *scr;
-  GtkWidget *label;
-  GtkWidget *table;
+    GtkWidget *entry;
+    GtkWidget *scr;
+    GtkWidget *label;
+    GtkWidget *table;
 
-  GtkTextTag *primary_user;
-  GtkTextTag *secondary_user;
+    GtkTextTag *primary_user;
+    GtkTextTag *secondary_user;
 
-  GtkTextIter  iter;
-  GtkTextMark *mark;
+    GtkTextIter  iter;
+    GtkTextMark *mark;
 
-  /* Get the structs from the application */
-  GM_window_widgets *gw = gnomemeeting_get_main_window (gm);
+    /* Get the structs from the application */
+    GM_window_widgets *gw = gnomemeeting_get_main_window (gm);
 
-  gw->chat_window = gtk_frame_new (_("Text Chat"));
+    gw->chat_window = gtk_frame_new (_("Text Chat"));
 
-  table = gtk_table_new (1, 3, FALSE);
+    table = gtk_table_new (1, 3, FALSE);
 
-  gtk_container_set_border_width (GTK_CONTAINER (table), 
-				  GNOME_PAD_SMALL);
-  gtk_container_add (GTK_CONTAINER (gw->chat_window), table);
+    gtk_container_set_border_width (GTK_CONTAINER (table), 
+                                    GNOME_PAD_SMALL);
+    gtk_container_add (GTK_CONTAINER (gw->chat_window), table);
 
-  scr = gtk_scrolled_window_new (NULL, NULL);
-  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scr),
-				  GTK_POLICY_AUTOMATIC,
-				  GTK_POLICY_ALWAYS);
-  gtk_widget_set_size_request (GTK_WIDGET (scr), 225, -1);
+    scr = gtk_scrolled_window_new (NULL, NULL);
+    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scr),
+                                    GTK_POLICY_AUTOMATIC,
+                                    GTK_POLICY_ALWAYS);
+    gtk_widget_set_size_request (GTK_WIDGET (scr), 225, -1);
 
-  gw->chat_view = gtk_text_view_new ();
-  gtk_text_view_set_editable (GTK_TEXT_VIEW (gw->chat_view), FALSE);
-  gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (gw->chat_view), GTK_WRAP_WORD);
+    gw->chat_view = gtk_text_view_new ();
+    gtk_text_view_set_editable (GTK_TEXT_VIEW (gw->chat_view), FALSE);
+    gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (gw->chat_view), GTK_WRAP_WORD);
 
-  gw->chat_buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (gw->chat_view));
+    gw->chat_buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (gw->chat_view));
 
-  gtk_text_buffer_get_end_iter (gw->chat_buffer, &iter);
+    gtk_text_buffer_get_end_iter (gw->chat_buffer, &iter);
   
-  mark = gtk_text_buffer_create_mark (gw->chat_buffer, "current-position", &iter, FALSE);
+    mark = gtk_text_buffer_create_mark (gw->chat_buffer, 
+                                        "current-position", &iter, FALSE);
 
-  gtk_text_buffer_create_tag (gw->chat_buffer, "primary-user",
-			      "foreground", "red", 
-                              "weight", 900, NULL); 
+    gtk_text_buffer_create_tag (gw->chat_buffer, "primary-user",
+                                "foreground", "red", 
+                                "weight", 900, NULL); 
 
-  gtk_text_buffer_create_tag (gw->chat_buffer, "secondary-user",
-			      "foreground", "blue", 
-                              "weight", 900, NULL);
+    gtk_text_buffer_create_tag (gw->chat_buffer, "secondary-user",
+                                "foreground", "blue", 
+                                "weight", 900, NULL);
 
-  gtk_container_add (GTK_CONTAINER (scr), gw->chat_view);
+    gtk_container_add (GTK_CONTAINER (scr), gw->chat_view);
   
-  gtk_table_attach (GTK_TABLE (table), GTK_WIDGET (scr), 
- 		    0, 1, 0, 1,
- 		    (GtkAttachOptions) (GTK_FILL | GTK_EXPAND),
- 		    (GtkAttachOptions) (GTK_FILL | GTK_EXPAND),
- 		    0, 0);
+    gtk_table_attach (GTK_TABLE (table), GTK_WIDGET (scr), 
+                      0, 1, 0, 1,
+                      (GtkAttachOptions) (GTK_FILL | GTK_EXPAND),
+                      (GtkAttachOptions) (GTK_FILL | GTK_EXPAND),
+                      0, 0);
 
-  label = gtk_label_new (_("Send Message:"));
-  gtk_misc_set_alignment (GTK_MISC (label), 1.0, 0.5);
-  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
-  gtk_table_attach (GTK_TABLE (table), GTK_WIDGET (label), 
-		    0, 1, 1, 2,
-		    (GtkAttachOptions) (GTK_FILL),
-		    (GtkAttachOptions) (GTK_FILL),
-		    0, 0);
+    label = gtk_label_new (_("Send Message:"));
+    gtk_misc_set_alignment (GTK_MISC (label), 1.0, 0.5);
+    gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
+    gtk_table_attach (GTK_TABLE (table), GTK_WIDGET (label), 
+                      0, 1, 1, 2,
+                      (GtkAttachOptions) (GTK_FILL),
+                      (GtkAttachOptions) (GTK_FILL),
+                      0, 0);
 
-  entry = gtk_entry_new ();
-  gtk_widget_set_size_request (GTK_WIDGET (entry), 225, -1);
-  gtk_table_attach (GTK_TABLE (table), GTK_WIDGET (entry), 
-		    0, 1, 2, 3,
-		    (GtkAttachOptions) (NULL),
-		    (GtkAttachOptions) (NULL),
-		    0, 0);
+    entry = gtk_entry_new ();
+    gtk_widget_set_size_request (GTK_WIDGET (entry), 225, -1);
+    gtk_table_attach (GTK_TABLE (table), GTK_WIDGET (entry), 
+                      0, 1, 2, 3,
+                      (GtkAttachOptions) (NULL),
+                      (GtkAttachOptions) (NULL),
+                      0, 0);
 
-  g_signal_connect (GTK_OBJECT (entry), "activate",
-                    G_CALLBACK (chat_entry_activate), gw->chat_view);
+    g_signal_connect (GTK_OBJECT (entry), "activate",
+                      G_CALLBACK (chat_entry_activate), gw->chat_view);
 
 }
