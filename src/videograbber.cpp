a5b86009fd007a07ad40d1899b502dd04e1b3928
Damien Sandras <dsandras@seconix.com>
2003-03-05 12:42:40 +0000

    Added workaround to bug in pwc 8.10.
    2003-03-05 Damien Sandras  <dsandras@seconix.com>
    
            * src/videograbber.cpp:	Added workaround to bug	in pwc	8.10.

--- src/videograbber.cpp
+++ src/videograbber.cpp
@@ -517,14 +517,14 @@ void GMVideoGrabber::VGOpen (void)
         if (!grabber->SetChannel(video_channel))
           error_code = 2;
       else
+	if (!grabber->SetFrameSizeConverter (width, height, FALSE))
+	   error_code = 5;
+      else
 	if (!grabber->SetColourFormatConverter ("YUV420P"))
 	  error_code = 3;
       else
 	if (!grabber->SetFrameRate (30))
 	  error_code = 4;
-      else
-	if (!grabber->SetFrameSizeConverter (width, height, FALSE))
-	   error_code = 5;
     }
 
 
