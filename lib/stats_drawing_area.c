70020cbdf7f489d1dffd30eb88ba31205e769c35
Julien Puydt <jpuydt@src.gnome.org>
2004-08-19 02:00:31 +0000

    StatsDrawingArea is in good shape

--- lib/stats_drawing_area.c
+++ lib/stats_drawing_area.c
@@ -1,4 +1,45 @@
+
+/*  stats_drawing_area.c
+ *
+ *  GnomeMeeting -- A Video-Conferencing application
+ *  Copyright (C) 2000-2002 Damien Sandras
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *
+ *  Authors: Damien Sandras <dsandras@seconix.com>
+ *           Julien Puydt <jpuydt@free.fr>
+ */
+
 #include "stats_drawing_area.h"
+
+#ifndef DISABLE_GNOME
+#include <gnome.h>
+#endif
+
+#ifndef _
+#ifdef DISABLE_GNOME
+#include <libintl.h>
+#define _(x) gettext(x)
+#ifdef gettext_noop
+#define N_(String) gettext_noop (String)
+#else
+#define N_(String) (String)
+#endif
+#endif
+#endif
 
 /* internal details of the object */
 
@@ -31,38 +72,43 @@ struct _StatsDrawingAreaClass {
 
 /* helper functions' declarations */
 
-static void stats_drawing_area_dispose (GObject *self);
+
+/* the three usual functions for GObjects */
 static void stats_drawing_area_finalize (GObject *self);
 static void stats_drawing_area_init (StatsDrawingArea *self);
+
+
+/* DESCRIPTION  : zeroes the cached data and the display
+ * PRE          : a non-NULL StatsDrawingArea
+ */
 static void stats_drawing_area_data_reset (StatsDrawingArea *self);
+
+
+/* DESCRIPTION  : gtk callback that triggers the redisplay of the
+ *                drawing.
+ * PRE          : the GtkWidget* should hide a StatsDrawingArea*
+ */
 static gboolean stats_drawing_area_exposed_cb (GtkWidget *,
 					       GdkEventExpose *,
 					       gpointer);
 
-/* helper functions' definitions */
+
+/* helper functions' implementation */
 
 
 static void
-stats_drawing_area_dispose (GObject *self)
+stats_drawing_area_finalize (GObject *obj)
 {
   GObjectClass *parent_class = NULL;
+  StatsDrawingArea *self = STATS_DRAWING_AREA (obj);
 
-  g_print ("Test object dispose\n");
-
-  parent_class = G_OBJECT_CLASS (g_type_class_peek_parent (STATS_DRAWING_AREA_GET_CLASS (self)));
-  parent_class->dispose (G_OBJECT (self));
-}
-
-
-static void
-stats_drawing_area_finalize (GObject *self)
-{
-  GObjectClass *parent_class = NULL;
-
-  g_print ("Test object dispose\n");
+  gdk_colormap_free_colors (self->colormap, self->colors, 6);
+  gdk_gc_unref (self->gc);
+  g_object_unref (G_OBJECT (self->pango_layout));
 
   parent_class = G_OBJECT_CLASS (g_type_class_peek_parent (STATS_DRAWING_AREA_GET_CLASS (self)));
-  parent_class->finalize (G_OBJECT (self));
+  if (parent_class->finalize)
+    parent_class->finalize (obj);
 }
 
 
@@ -107,11 +153,9 @@ stats_drawing_area_init (StatsDrawingArea *self)
 static void
 stats_drawing_area_class_init (StatsDrawingAreaClass *klass)
 {
-  GObjectClass *object_klass = NULL;
+  GObjectClass *gobject_klass = G_OBJECT_CLASS (klass);
 
-  object_klass = G_OBJECT_CLASS (klass);
-  object_klass->dispose = stats_drawing_area_dispose;
-  object_klass->finalize = stats_drawing_area_finalize;
+  gobject_klass->finalize = stats_drawing_area_finalize;
 }
 
 static void
@@ -313,7 +357,7 @@ stats_drawing_area_exposed_cb (GtkWidget *widget,
   /* Text */
   
   pango_text =
-    g_strdup_printf ("Total: %.2f MB", /* FIXME: gettextize */
+    g_strdup_printf (_("Total: %.2f MB"),
 		     (float) (self->last_video_octets_transmitted
 			      + self->last_audio_octets_transmitted
 			      + self->last_video_octets_received
