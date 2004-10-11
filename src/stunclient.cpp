b2fe48b74905fdbf8e40ec082e5f9ffc365a3250
Damien Sandras <dsandras@gnome.org>
2004-10-11 21:01:32 +0000

    Enhanced the StunClient so that it can also serve as a STUN configurator.
    2004-10-11  Damien Sandras  <dsandras@gnome.org>
    
    	* src/stunclient.cpp: Enhanced the StunClient so that it can also
    	serve as a STUN configurator.

--- src/stunclient.cpp
+++ src/stunclient.cpp
@@ -92,6 +92,12 @@ void GMStunClient::Main ()
   druid_window = GnomeMeeting::Process ()->GetDruidWindow ();
 
   gchar *prefered_method = NULL;
+  gchar *primary_text = NULL;
+  gchar *dialog_text = NULL;
+
+  GtkWidget *dialog = NULL;
+  GtkWidget *dialog_label = NULL;
+
   char *name [] = { N_("Unknown NAT"), N_("Open NAT"),
     N_("Cone NAT"), N_("Restricted NAT"), N_("Port Restricted NAT"),
     N_("Symmetric NAT"), N_("Symmetric Firewall"), N_("Blocked"),
@@ -131,7 +137,7 @@ void GMStunClient::Main ()
       gnomemeeting_threads_leave ();
     }
   } 
-  /* Only detects */
+  /* Only detects and configure */
   else if (!reg && !stun_host.IsEmpty ()) {
 
     PSTUNClient stun (stun_host,
@@ -145,26 +151,87 @@ void GMStunClient::Main ()
     switch (stun.GetNatType ())
       {
       case 0:
-	prefered_method = g_strdup (_("GnomeMeeting couldn't detect the type of NAT you are using, the most appropriate method if your router doesn't support H.323 is probably to use IP Translation or a gatekeeper in proxy mode."));
+	prefered_method = g_strdup (_("GnomeMeeting couldn't detect the type of NAT you are using, the most appropriate method if your router doesn't natively support H.323 is probably to forward the required ports and to use IP translation or to use an H.323 gatekeeper in proxy mode."));
 	break;
 
       case 5:
-	prefered_method = g_strdup (_("GnomeMeeting detected Symmetric NAT. The most appropriate method if your router doesn't support H.323 is to use IP Translation or a gatekeeper in proxy mode."));
+	prefered_method = g_strdup (_("GnomeMeeting detected Symmetric NAT. The most appropriate method if your router doesn't natively support H.323 is to forward the required ports to change your Symmetric NAT into Cone NAT and run this test again. It should report Cone NAT, and allow GnomeMeeting to be used with a STUN Server."));
 	break;
 	
       default:
-	prefered_method = g_strdup (_("Using a STUN server is most probably the most appropriate method if your router doesn't support H.323."));
+	prefered_method = g_strdup_printf (_("GnomeMeeting detected %s. Using a STUN server is most probably the most appropriate method if your router doesn't natively support H.323.\n\nEnable STUN Support?"), (const char *) nat_type);
       }
 
     gnomemeeting_threads_enter ();
-    gnomemeeting_message_dialog (GTK_WINDOW (druid_window), 
-				 _("NAT detection successfull"),
-				 _("Detected %s using %s.\n\n%s\n\nPlease see the FAQ on http://www.gnomemeeting.org for more information and possibilities."), 
-				 (const char *) nat_type,
-				 (const char *) stun_host,
-				 prefered_method);
+    if (stun.GetNatType () != 0 && stun.GetNatType () != 5) {
+      
+      dialog = 
+	gtk_dialog_new_with_buttons (_("NAT Detection Successfull"),
+				     GTK_WINDOW (druid_window),
+				     GTK_DIALOG_MODAL,
+				     GTK_STOCK_NO,
+				     GTK_RESPONSE_NO,
+				     GTK_STOCK_YES,
+				     GTK_RESPONSE_YES,
+				     NULL);
+    }
+    else {
+
+      dialog = 
+	gtk_dialog_new_with_buttons (_("NAT Detection Successfull"),
+				     GTK_WINDOW (druid_window),
+				     GTK_DIALOG_MODAL,
+				     GTK_STOCK_OK,
+				     GTK_RESPONSE_ACCEPT,
+				     NULL);
+    }
+    
+    primary_text =
+      g_strdup_printf ("<span weight=\"bold\" size=\"larger\">%s</span>",
+		       _("The type of NAT was successfully detected"));
+
+    dialog_text =
+      g_strdup_printf ("%s\n\n%s", primary_text, prefered_method);
+
+    gtk_window_set_title (GTK_WINDOW (dialog), "");
+    dialog_label = gtk_label_new (NULL);
+    gtk_label_set_markup (GTK_LABEL (dialog_label),
+			  dialog_text);
+    gtk_label_set_line_wrap (GTK_LABEL (dialog_label), TRUE);
+    gtk_container_add (GTK_CONTAINER (GTK_DIALOG (dialog)->vbox),
+		       dialog_label);
+    gtk_widget_show (dialog_label);
+    gnomemeeting_threads_dialog_show (dialog);
+
+    switch (gtk_dialog_run (GTK_DIALOG (dialog))) {
+
+    case GTK_RESPONSE_YES:
+
+      gm_conf_set_string (NAT_KEY "stun_server", "stun.voxgratia.org");
+      gm_conf_set_bool (NAT_KEY "enable_stun_support", TRUE);
+      
+      ((H323EndPoint *) endpoint)->SetSTUNServer ("stun.voxgratia.org");
+      
+      gm_history_window_insert (history_window, _("Set STUN server to %s"), 
+				"stun.voxgratia.org");
+      
+      break;
+
+    case GTK_RESPONSE_NO:
+      
+      gm_conf_set_bool (NAT_KEY "enable_stun_support", FALSE);
+      
+      ((H323EndPoint *) endpoint)->SetSTUNServer (PString ());
+
+      gm_history_window_insert (history_window, _("Removed STUN server"));
+
+      break;
+    }
+    gtk_widget_destroy (dialog);
     gnomemeeting_threads_leave ();
 
+    g_free (primary_text);
+    g_free (dialog_text);
     g_free (prefered_method);
   }
 }
