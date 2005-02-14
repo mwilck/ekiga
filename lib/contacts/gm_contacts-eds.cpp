71c415f6498552e0a2b13ee25e3ff61ce5866f62
Damien Sandras <dsandras@src.gnome.org>
2005-02-14 19:16:37 +0000

    Fixed typo. Thanks Kilian.

--- lib/contacts/gm_contacts-eds.cpp
+++ lib/contacts/gm_contacts-eds.cpp
@@ -409,7 +409,7 @@ gnomemeeting_local_addressbook_get_contacts (GmAddressbook *addbook,
 			       categorie);
 
   if (cpt == 0)
-    queries [cpt++] = e_book_query_field_exists (E_CONTACT_FULLNAME);
+    queries [cpt++] = e_book_query_field_exists (E_CONTACT_FULL_NAME);
 
   query = e_book_query_or (cpt, queries, TRUE);
 
