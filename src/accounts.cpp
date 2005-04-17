d10c9be34f5bac053551b0e09fbc2839191123c7
Damien Sandras <dsandras@src.gnome.org>
2005-04-17 10:26:15 +0000

    Fixed crash in regex checking.

--- src/accounts.cpp
+++ src/accounts.cpp
@@ -232,7 +232,7 @@ gm_aw_edit_account_dialog_run (GtkWidget *accounts_window,
   GtkWidget *menu = NULL;
   GtkWidget *item = NULL;
 
-  PRegularExpression regex ("^[a-z0-9][a-z0-9.\\- ]*$", 
+  PRegularExpression regex ("^[a-z0-9][-.a-z0-9 ]*$", 
 			    PRegularExpression::IgnoreCase);
 
   PString username;
