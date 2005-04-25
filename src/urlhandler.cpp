c23d39b811a2f5028942968f29a3ead965b00be7
Damien Sandras <dsandras@src.gnome.org>
2005-04-25 19:34:14 +0000

    Added callto support back directly in gnomemeeting.

--- src/urlhandler.cpp
+++ src/urlhandler.cpp
@@ -52,6 +52,8 @@
 #include "contacts/gm_contacts.h"
 #include "gm_conf.h"
 
+#include <ptclib/pils.h>
+
 
 /* Declarations */
 static gint
@@ -405,6 +407,37 @@ void GMURLHandler::Main ()
     return;
   }
 
+  if (url.GetType () == "callto") { 
+
+    PILSSession ils;
+    int part1 = 0;
+    int part2 = 0;
+    int part3 = 0;
+    int part4 = 0;
+    gchar *ip = NULL;
+    
+    if (ils.Open (url.GetCalltoServer ())) {
+
+      PILSSession::RTPerson person;
+
+      if (ils.SearchPerson (url.GetCalltoEmail (), person)) {
+
+	part1 = (person.sipAddress & 0xff000000) >> 24;
+	part2 = (person.sipAddress & 0x00ff0000) >> 16;
+	part3 = (person.sipAddress & 0x0000ff00) >> 8;
+	part4 = person.sipAddress & 0x000000ff;
+
+	ip = 
+	  g_strdup_printf ("%s:%d.%d.%d.%d:%d", 
+			   (const char *) person.sprotid[0],
+			   part4, part3, part2, part1, 
+			   (int) *person.sport);
+	call_address = ip;
+	g_free (ip);
+      }
+    }
+  }
+  
 
   /* Update the history */
   gnomemeeting_threads_enter ();
