bba8e9c8cb91f74d01caf4d81ab188861034e3ea
Damien Sandras <dsandras@src.gnome.org>
2005-11-29 21:07:12 +0000

    Converted to GM 2.00 and SIP support.

--- src/tools.cpp
+++ src/tools.cpp
@@ -49,10 +49,38 @@
 #include "gnome_prefs_window.h"
 
 
-static void pc2phone_window_response_cb (GtkWidget *, gint, gpointer);
+typedef struct _GmPC2PhoneWindow
+{
+  GtkWidget *login_entry;
+  GtkWidget *pin_entry;
+  GtkWidget *use_service_toggle;
+} GmPC2PhoneWindow;
 
-static void pc2phone_consult_cb (GtkWidget *, gpointer);
+#define GM_PC2PHONE_WINDOW(x) (GmPC2PhoneWindow *) (x)
 
+
+/* Declarations */
+
+/* GUI Functions */
+
+
+/* DESCRIPTION  : /
+ * BEHAVIOR     : Frees a GmPC2PhoneWindow and its content.
+ * PRE          : A non-NULL pointer to a GmPC2PhoneWindow structure.
+ */
+static void gm_pcw_destroy (gpointer);
+
+
+/* DESCRIPTION  : /
+ * BEHAVIOR     : Returns a pointer to the private GmPC2PhoneWindow structure
+ *                used by the pc2phone window GMObject.
+ * PRE          : The given GtkWidget pointer must be a pc2phone window 
+ * 		  GMObject.
+ */
+static GmPC2PhoneWindow *gm_pcw_get_pcw (GtkWidget *);
+
+
+/* Callbacks */
 
 /* DESCRIPTION  :  This callback is called when the user validates an answer
  *                 to the PC-To-Phone window.
@@ -60,39 +88,9 @@ static void pc2phone_consult_cb (GtkWidget *, gpointer);
  *                 (if not cancel), ie change the settings and register to gk.
  * PRE          :  /
  */
-static void
-pc2phone_window_response_cb (GtkWidget *w,
-			     gint response,
-			     gpointer data)
-{
-  GMEndPoint *ep = NULL;
-  GMH323EndPoint *h323EP = NULL;
-
-  ep = GnomeMeeting::Process ()->Endpoint ();
-  h323EP = ep->GetH323EndPoint ();
-  
-  if (response != 1)
-    gnomemeeting_window_hide (w);
-  
-
-  if (response == 1 || response == 0) {
-
-    if (gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (data))) {
-	
-      /* The Username and PIN already are correct, update the other settings */
-      gm_conf_set_bool (H323_KEY "enable_fast_start", TRUE);
-      gm_conf_set_bool (H323_KEY "enable_h245_tunneling", TRUE);
-      gm_conf_set_bool (H323_KEY "enable_early_h245", TRUE);
-      gm_conf_set_string (H323_KEY "gatekeeper_host", "gk.ast.diamondcard.us");
-      gm_conf_set_int (H323_KEY "gatekeeper_registering_method", 1);
-    }
-    else
-      gm_conf_set_int (H323_KEY "gatekeeper_registering_method", 0);
-    
-    /* Register the current Endpoint to the Gatekeeper */
-    //h323EP->GatekeeperRegister ();
-  }
-}
+static void pc2phone_window_response_cb (GtkWidget *, 
+					 gint, 
+					 gpointer);
 
 
 /* DESCRIPTION  :  This callback is called when the user clicks on the link
@@ -104,19 +102,113 @@ pc2phone_window_response_cb (GtkWidget *w,
  * 				          == 2 : calls history
  * 					  == 3 : get an account
  */
+static void pc2phone_consult_cb (GtkWidget *, 
+				 gpointer);
+
+
+/* Implementation */
+static void
+gm_pcw_destroy (gpointer pcw)
+{
+  g_return_if_fail (pcw != NULL);
+
+  delete ((GmPC2PhoneWindow *) pcw);
+}
+
+
+static GmPC2PhoneWindow *
+gm_pcw_get_pcw (GtkWidget *pc2phone_window)
+{
+  g_return_val_if_fail (pc2phone_window != NULL, NULL);
+
+  return GM_PC2PHONE_WINDOW (g_object_get_data (G_OBJECT (pc2phone_window), "GMObject"));
+}
+
+
+static void
+pc2phone_window_response_cb (GtkWidget *w,
+			     gint response,
+			     gpointer data)
+{
+  GMEndPoint *ep = NULL;
+  
+  GmAccount *account = NULL;
+  GmPC2PhoneWindow *pcw = NULL;
+
+  gboolean new_account = FALSE;
+  
+  g_return_if_fail (data != NULL);
+
+  pcw = gm_pcw_get_pcw (GTK_WIDGET (data));
+  
+  g_return_if_fail (pcw != NULL);
+  
+  ep = GnomeMeeting::Process ()->Endpoint ();
+  account = gnomemeeting_get_account ("eugw.ast.diamondcard.us");
+  if (account == NULL) {
+
+    account = gm_account_new ();
+    account->account_name = g_strdup ("GnomeMeeting PC-To-Phone");
+    account->host = g_strdup ("eugw.ast.diamondcard.us");
+    account->domain = g_strdup ("eugw.ast.diamondcard.us");
+    account->protocol_name = g_strdup ("SIP");
+  
+    new_account = TRUE;
+  }
+  
+  if (response != 1)
+    gnomemeeting_window_hide (w);
+  
+  if (response == 1 || response == 0) {
+
+    if (account->login)
+      g_free (account->login);
+    if (account->password)
+      g_free (account->password);
+    
+    account->login = 
+      g_strdup (gtk_entry_get_text (GTK_ENTRY (pcw->login_entry)));
+    account->password = 
+      g_strdup (gtk_entry_get_text (GTK_ENTRY (pcw->pin_entry)));
+    account->enabled =
+      gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (pcw->use_service_toggle));
+
+    /* Update the account or create it */
+    if (new_account)
+      gnomemeeting_account_add (account);
+    else
+      gnomemeeting_account_modify (account);
+    
+    /* Register the current Endpoint to the Gatekeeper */
+    ep->Register (account);
+  }
+
+  gm_account_delete (account);
+}
+
+
 static void
 pc2phone_consult_cb (GtkWidget *widget,
 		     gpointer data)
 {
-  gchar *account = NULL;
-  gchar *pin = NULL;
+  GmPC2PhoneWindow *pcw = NULL;
+
+  GtkWidget *pc2phone_window = NULL;
+  
+  const char *account = NULL;
+  const char *pin = NULL;
+
   gchar *url = NULL;
 #ifdef DISABLE_GNOME
   gchar *command = NULL;
 #endif
 
-  account = gm_conf_get_string (H323_KEY "gatekeeper_login");
-  pin = gm_conf_get_string (H323_KEY "gatekeeper_password");
+  pc2phone_window = GnomeMeeting::Process ()->GetPC2PhoneWindow ();
+  pcw = gm_pcw_get_pcw (pc2phone_window);
+
+  account = gtk_entry_get_text (GTK_ENTRY (pcw->login_entry));
+  pin = gtk_entry_get_text (GTK_ENTRY (pcw->pin_entry));
+
 
   if (account == NULL || pin == NULL)
     return; /* no account configured yet */
@@ -138,8 +230,6 @@ pc2phone_consult_cb (GtkWidget *widget,
   gnome_url_show (url, NULL);
 #endif
 
-  g_free (account);
-  g_free (pin);
   g_free (url);
 }
 
@@ -147,27 +237,36 @@ pc2phone_consult_cb (GtkWidget *widget,
 GtkWidget *
 gm_pc2phone_window_new ()
 {
+  GmAccount *account = NULL;
+
+  GmPC2PhoneWindow *pcw = NULL;
+  
   GtkWidget *window = NULL;
   GtkWidget *button = NULL;
   GtkWidget *label = NULL;
-  GtkWidget *use_service_button = NULL;
-  GtkWidget *entry = NULL;
   GtkWidget *vbox = NULL;
   GtkWidget *subsection = NULL;
 
   GdkPixbuf *pixbuf = NULL;
 
   gchar *txt = NULL;
-  gchar *gatekeeper = NULL;
-  gint method = 0;
   
+
+  /* Get the PC-To-Phone account, if any */
+  account = gnomemeeting_get_account ("eugw.ast.diamondcard.us");
+
+  
+  /* Build the window */
   window = gtk_dialog_new ();
   gtk_dialog_add_buttons (GTK_DIALOG (window),
 			  GTK_STOCK_APPLY,  1,
 			  GTK_STOCK_CANCEL, 2,
 			  GTK_STOCK_OK, 0,
 			  NULL);
-  
+
+  pcw = new GmPC2PhoneWindow ();
+  g_object_set_data_full (G_OBJECT (window), "GMObject", 
+			  pcw, (GDestroyNotify) gm_pcw_destroy);
   g_object_set_data_full (G_OBJECT (window), "window_name",
 			  g_strdup ("pc_to_phone_window"), g_free);
   
@@ -178,47 +277,69 @@ gm_pc2phone_window_new ()
   gtk_window_set_icon (GTK_WINDOW (window), pixbuf);
   g_object_unref (pixbuf);
 
-
+  /* Introduction label */
   label = gtk_label_new (_("You can make calls to regular phones and cell numbers worldwide using GnomeMeeting. To enable this, you need to register an account using the URL below, then enter your Account number and PIN, and finally enable registering to the GnomeMeeting PC-To-Phone service.\n\nPlease make sure you are using the URL below to get your account otherwise the service will not work."));
   gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
-  
   gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->vbox), label,
 		      FALSE, FALSE, 20);
 
-
+  /* Settings */
   vbox = GTK_DIALOG (window)->vbox;
-  
   subsection =
     gnome_prefs_subsection_new (window, vbox,
-				_("PC-To-Phone Settings"), 2, 1);
+				_("PC-To-Phone Settings"), 3, 2);
+  
+  label = gtk_label_new_with_mnemonic (_("Account _number:"));
+  gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
+  gtk_table_attach (GTK_TABLE (subsection), label, 0, 1, 0, 1,
+		    (GtkAttachOptions) (GTK_FILL),
+		    (GtkAttachOptions) (GTK_FILL),
+		    0, 0);
 
-  gnome_prefs_entry_new (subsection, _("Account _number:"), H323_KEY "gatekeeper_login", _("Use your MicroTelco account number"), 1, false);
+  pcw->login_entry = gtk_entry_new ();
+  if (account && account->login)
+    gtk_entry_set_text (GTK_ENTRY (pcw->login_entry), account->login);
+  gtk_label_set_mnemonic_widget (GTK_LABEL (label), pcw->login_entry);
+  gtk_table_attach (GTK_TABLE (subsection), pcw->login_entry, 1, 2, 0, 1,
+		    (GtkAttachOptions) (GTK_FILL),
+		    (GtkAttachOptions) (GTK_FILL),
+		    0, 0);
+  
+  label = gtk_label_new_with_mnemonic (_("_Pin:"));
+  gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+  gtk_label_set_justify (GTK_LABEL (label), GTK_JUSTIFY_LEFT);
+  gtk_table_attach (GTK_TABLE (subsection), label, 0, 1, 1, 2,
+		    (GtkAttachOptions) (GTK_FILL),
+		    (GtkAttachOptions) (GTK_FILL),
+		    0, 0);
+  
+  pcw->pin_entry = gtk_entry_new ();
+  if (account && account->password)
+    gtk_entry_set_text (GTK_ENTRY (pcw->pin_entry), account->password);
+  gtk_label_set_mnemonic_widget (GTK_LABEL (label), pcw->pin_entry);
+  gtk_table_attach (GTK_TABLE (subsection), pcw->pin_entry, 1, 2, 1, 2,
+		    (GtkAttachOptions) (GTK_FILL),
+		    (GtkAttachOptions) (GTK_FILL),
+		    0, 0);
 
-  entry =
-    gnome_prefs_entry_new (subsection, _("_Pin:"), H323_KEY "gatekeeper_password", _("Use your MicroTelco PIN"), 2, false);
-  gtk_entry_set_visibility (GTK_ENTRY (entry), FALSE);
-
-
-  /* Use service or not */
-  use_service_button =
+  pcw->use_service_toggle =
     gtk_check_button_new_with_label (_("Use PC-To-Phone service"));
-  gatekeeper = gm_conf_get_string (H323_KEY "gatekeeper_host");
-  method =  gm_conf_get_int (H323_KEY "gatekeeper_registering_method");
-  if (method == 1 && gatekeeper 
-      && !strcmp (gatekeeper, "gk.ast.diamondcard.us"))
-    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (use_service_button), TRUE);
-  gtk_box_pack_start (GTK_BOX (vbox), GTK_WIDGET (use_service_button),
-		      FALSE, TRUE, 0);
-  g_free (gatekeeper);
-  
-  
+  if (account && account->enabled)
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (pcw->use_service_toggle), 
+				  TRUE);
+  gtk_table_attach (GTK_TABLE (subsection), 
+		    pcw->use_service_toggle, 0, 2, 2, 3,
+		    (GtkAttachOptions) (GTK_FILL),
+		    (GtkAttachOptions) (GTK_FILL),
+		    0, 0);
+
   /* Explanation label */
   label =
     gtk_label_new (_("Click on one of the following links to get more information about your existing GnomeMeeting PC-To-Phone account, or to create a new account."));
   gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
   gtk_box_pack_start (GTK_BOX (vbox), GTK_WIDGET (label), FALSE, FALSE, 20);
 
-  
   /* Get an account, good idea */
   button = gtk_button_new ();
   label = gtk_label_new (NULL);
@@ -231,7 +352,6 @@ gm_pc2phone_window_new ()
   gtk_box_pack_start (GTK_BOX (vbox), GTK_WIDGET (button), FALSE, FALSE, 0);
   g_signal_connect (GTK_OBJECT (button), "clicked",
 		    G_CALLBACK (pc2phone_consult_cb), GINT_TO_POINTER (3));
-
 
   /* Recharge account */
   button = gtk_button_new ();
@@ -246,7 +366,6 @@ gm_pc2phone_window_new ()
   g_signal_connect (GTK_OBJECT (button), "clicked",
 		    G_CALLBACK (pc2phone_consult_cb), GINT_TO_POINTER (0));
 
-  
   /* Consult the balance history */
   button = gtk_button_new ();
   label = gtk_label_new (NULL);
@@ -260,7 +379,6 @@ gm_pc2phone_window_new ()
   g_signal_connect (GTK_OBJECT (button), "clicked",
 		    G_CALLBACK (pc2phone_consult_cb), GINT_TO_POINTER (1));
 
-  
   /* Consult the calls history */
   button = gtk_button_new ();
   label = gtk_label_new (NULL);
@@ -277,12 +395,15 @@ gm_pc2phone_window_new ()
   g_signal_connect (GTK_OBJECT (window), 
 		    "response", 
 		    G_CALLBACK (pc2phone_window_response_cb),
-		    (gpointer) use_service_button);
+		    (gpointer) window);
 
   g_signal_connect (GTK_OBJECT (window), "delete-event", 
                     G_CALLBACK (delete_window_cb), NULL);
   
   gtk_widget_show_all (GTK_WIDGET (GTK_DIALOG (window)->vbox));
+
+  if (account)
+    gm_account_delete (account);
   
   return window;
 }
