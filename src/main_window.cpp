1b845f8b64138f3b5675bb4ceac9d902190e51ec
Damien Sandras <dsandras@src.gnome.org>
2005-11-30 22:32:09 +0000

    Fixed leak.

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -2599,6 +2599,8 @@ gm_main_window_update_video (GtkWidget *main_window,
 				   GDK_INTERP_BILINEAR:GDK_INTERP_NEAREST);
       else
 	zlsrc_pic = gdk_pixbuf_copy (lsrc_pic);
+
+      g_object_unref (lsrc_pic);
     }
   }
   
