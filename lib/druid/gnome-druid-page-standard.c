694c23da19459ef5f0c35058b1a7575fb8644893
Damien Sandras <dsandras@src.gnome.org>
2005-12-04 16:25:48 +0000

    More import from the opal branch.

--- lib/druid/gnome-druid-page-standard.c
+++ lib/druid/gnome-druid-page-standard.c
@@ -22,18 +22,6 @@
 
 #include "gnome-druid.h"
 
-#ifndef _
-#ifdef DISABLE_GNOME
-#include <libintl.h>
-#define _(x) gettext(x)
-#ifdef gettext_noop
-#define N_(String) gettext_noop (String)
-#else
-#define N_(String) (String)
-#endif
-#endif
-#endif
-
 /* FIXME-orig: Are these includes needed */
 #include <gtk/gtklabel.h>
 #include <gtk/gtklayout.h>
