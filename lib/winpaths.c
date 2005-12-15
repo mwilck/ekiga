20f9084d9b548567938856dfad42fb6e261da4bf
Julien Puydt <jpuydt@src.gnome.org>
2005-12-15 12:43:33 +0000

    Simplification in winpaths...  Thank you glib developpers \!

--- lib/winpaths.c
+++ lib/winpaths.c
@@ -1,4 +1,4 @@
-#include <process.h>
+#include <glib/gwin32.h>
 
 #include "winpaths.h"
 
@@ -7,38 +7,8 @@ win32_basedir ()
 {
   static gchar *result = NULL;
 
-  if (!result) {
-
-    gchar *fullpath = NULL;
-
-    if (G_WIN32_HAVE_WIDECHAR_API ()) {
-
-      wchar_t winstall_dir [MAXPATHLEN];
-
-      if (GetModuleFileNameW (NULL, winstall_dir,
-			      MAXPATHLEN) > 0) {
-
-	fullpath = g_utf16_to_utf8 (winstall_dir, -1,
-				    NULL, NULL, NULL);
-      }
-    } else {
-
-      gchar ainstall_dir [MAXPATHLEN];
-
-      if (GetModuleFileNameA (NULL, ainstall_dir,
-			      MAXPATHLEN) > 0) {
-
-	fullpath = g_locale_to_utf8 (ainstall_dir, -1,
-				     NULL, NULL, NULL);
-      }
-    }
-
-    if (fullpath) {
-
-      result = g_path_get_dirname (fullpath);
-      g_free (fullpath);
-    }
-  }
+  if (!result)
+    result = g_win32_get_package_installation_directory (NULL, NULL);
 
   return result;
 }
