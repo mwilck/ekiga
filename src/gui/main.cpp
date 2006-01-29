b7da5b8322ebaeda2fe49dfe8a4dc1b726637d94
Damien Sandras <dsandras@src.gnome.org>
2006-01-29 17:47:33 +0000

    Fixed hack from Jan Schampera.

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -3620,7 +3620,7 @@ gm_main_window_set_account_info (GtkWidget *main_window,
   g_return_if_fail (mw != NULL);
 
   info = 
-    g_strdup_printf ("<b> \n</b><small>%s %d</small>",
+    g_strdup_printf ("<b>\n </b><small>%s %d</small>",
 		     _("Registered accounts:"),
 		     registered_accounts);
 		   
