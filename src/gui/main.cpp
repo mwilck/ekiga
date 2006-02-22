e69822b8bbcdef66360c793ff03f4ca512e10f2c
Damien Sandras <dsandras@src.gnome.org>
2006-02-22 19:29:43 +0000

    Applied patch from Brian Lu <brian lu sun com> to fix full screen on big endian machines.

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -2872,10 +2872,10 @@ gm_main_window_update_video (GtkWidget *main_window,
   case FULLSCREEN:
 
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-    rmask = 0xff000000;
-    gmask = 0x00ff0000;
-    bmask = 0x0000ff00;
-    amask = 0x000000ff;
+    rmask = 0x00ff0000;
+    gmask = 0x0000ff00;
+    bmask = 0x000000ff;
+    amask = 0x00000000;
 #else
     rmask = 0x000000ff;
     gmask = 0x0000ff00;
