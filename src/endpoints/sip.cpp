3b6272453ab8b8a2e58066c93e25ef1482c74b15
Damien Sandras <dsandras@src.gnome.org>
2006-03-19 16:06:24 +0000

    Changed timers to RFC3261 compliant values.

--- src/endpoints/sip.cpp
+++ src/endpoints/sip.cpp
@@ -93,7 +93,7 @@ GMSIPEndpoint::Init ()
   SetInviteTimeout (PTimeInterval (0, 6));
   SetNonInviteTimeout (PTimeInterval (0, 6));
   SetNATBindingTimeout (PTimeInterval (0, binding_timeout));
-  SetRetryTimeouts (250, 10000);
+  SetRetryTimeouts (500, 4000);
   SetMaxRetries (8);
 
 
