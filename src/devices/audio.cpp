504c1e419d36201362995b29b5c8225385ae2aa6
Damien Sandras <dsandras@src.gnome.org>
2007-02-04 21:33:50 +0000

    Use more internal buffers for sound events. Fixes issues with some soundcards.
    svn path=/trunk/; revision=4842

--- src/devices/audio.cpp
+++ src/devices/audio.cpp
@@ -203,7 +203,7 @@ void GMSoundEvent::Main ()
 
       if (channel) {
 
-	channel->SetBuffers (640, 2);
+	channel->SetBuffers (320, 4);
 	
 	buffer.SetSize (wav->GetDataLength ());
 	memset (buffer.GetPointer (), '0', buffer.GetSize ());
@@ -308,11 +308,7 @@ void GMAudioRP::Main ()
     
     nbr_opened_channels++;
 
-#ifndef WIN32
-    channel->SetBuffers (640, 2);
-#else
-    channel->SetBuffers (640, 3);
-#endif
+    channel->SetBuffers (320, 4);
     
     while (!stop) {
       
