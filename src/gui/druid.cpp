4595e657cd589addebb7a3a776c46a8a544858c4
Jan Schampera <jschamp@src.gnome.org>
2007-03-10 18:46:59 +0000

    Backport from HEAD (#413979, Julien Puydt)
    svn path=/branches/H_Release/; revision=4945

--- src/gui/druid.cpp
+++ src/gui/druid.cpp
@@ -1822,6 +1822,7 @@ prepare_audio_devices_page_cb (GnomeDruidPage *page,
   
   gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (dw->audio_test_button),
 				FALSE);
+  gtk_widget_set_sensitive (GTK_WIDGET (dw->audio_test_button), TRUE);
   
   if (dw->audio_manager)
     audio_manager = 
@@ -1843,8 +1844,6 @@ prepare_audio_devices_page_cb (GnomeDruidPage *page,
     devices += PString (_("No device found"));
     gtk_widget_set_sensitive (GTK_WIDGET (dw->audio_test_button), FALSE);
   }
-  else
-    gtk_widget_set_sensitive (GTK_WIDGET (dw->audio_test_button), TRUE);
   
   array = devices.ToCharArray ();
   gm_dw_option_menu_update (dw->audio_player, array, player);
@@ -1857,8 +1856,6 @@ prepare_audio_devices_page_cb (GnomeDruidPage *page,
     devices += PString (_("No device found"));
     gtk_widget_set_sensitive (GTK_WIDGET (dw->audio_test_button), FALSE);
   }
-  else 
-    gtk_widget_set_sensitive (GTK_WIDGET (dw->audio_test_button), TRUE);
   
   array = devices.ToCharArray ();
   gm_dw_option_menu_update (dw->audio_recorder, array, recorder);
