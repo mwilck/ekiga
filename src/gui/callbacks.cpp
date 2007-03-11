0b6c8e4b9cd0612ebbead25a9d6d8f76cc01745b
Damien Sandras <dsandras@src.gnome.org>
2007-03-11 11:41:20 +0000

    Added 2007 to the copyright.
    svn path=/branches/gnome-2-14/; revision=4952

--- src/gui/callbacks.cpp
+++ src/gui/callbacks.cpp
@@ -197,7 +197,7 @@ GNU GPL for all the rest of the software thus combined.";
   gtk_show_about_dialog (GTK_WINDOW (parent_window),
 		"name", "Ekiga",
 		"version", VERSION,
-		"copyright", "Copyright © 2000-2006 Damien Sandras",
+		"copyright", "Copyright © 2000-2007 Damien Sandras",
 		"authors", authors,
 		"documenters", documenters,
 		"translator-credits", translator_credits,
