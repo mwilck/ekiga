cd3b1d2d7cf5bd9c3c13362e49e75e92811312e0
Damien Sandras <dsandras@src.gnome.org>
2007-03-15 21:06:15 +0000

    Fixed several possible causes of crash.
    svn path=/branches/gnome-2-14/; revision=4971

--- lib/gmcontacts/gmcontacts-avahi.cpp
+++ lib/gmcontacts/gmcontacts-avahi.cpp
@@ -501,8 +501,8 @@ GMZeroconfBrowser::ResolveCallback (AvahiServiceResolver *r,
      * in the contact list */
     if (tmp_list && tmp_list->data) {
       
-      contacts = g_slist_remove (contacts, GM_CONTACT (tmp_list->data));
       gmcontact_delete (GM_CONTACT (tmp_list->data));
+      contacts = g_slist_remove (contacts, tmp_list);
     }
     
     contact->state = CONTACT_AVAILABLE;
@@ -583,13 +583,12 @@ GMZeroconfBrowser::BrowseCallback (AvahiServiceBrowser *b,
 
       contact = GM_CONTACT (l->data);
       
-      if (contact && contact->fullname) {
+      if (contact && contact->fullname && name) {
 
 	if (!strcmp (contact->fullname, name)) {
 
-	  contacts = g_slist_remove (contacts, 
-				     (gpointer) contact);
-	  gmcontact_delete (contact);
+          gmcontact_delete (GM_CONTACT (l->data));
+          contacts = g_slist_remove (contacts, l);
 	}
       }
     }
