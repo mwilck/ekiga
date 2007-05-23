6442c3492d72f964472c16ad80291fa11004a0b7
Damien Sandras <dsandras@src.gnome.org>
2007-05-23 20:01:46 +0000

    Reduced ACK timeout.
    svn path=/trunk/; revision=5158

--- src/endpoints/sip.cpp
+++ src/endpoints/sip.cpp
@@ -95,6 +95,7 @@ GMSIPEndpoint::Init ()
 
 
   /* Timeouts */
+  SetAckTimeout (PTimeInterval (0, 5));
   SetPduCleanUpTimeout (PTimeInterval (0, 1));
   SetInviteTimeout (PTimeInterval (0, 6));
   SetNonInviteTimeout (PTimeInterval (0, 6));
