37143b299858247d78958f56556d1086a70fd1d0
Damien Sandras <dsandras@src.gnome.org>
2007-08-21 20:41:32 +0000

    More code from Julien. kab disabled for now.
    svn path=/trunk/; revision=5440

---
+++ lib/engine/addressbook/ldap/ldap-book.cpp
@@ -0,0 +1,543 @@
+
+/* Ekiga -- A VoIP and Video-Conferencing application
+ * Copyright (C) 2000-2007 Damien Sandras
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ * Ekiga is licensed under the GPL license and as a special exception,
+ * you have permission to link or otherwise combine this program with the
+ * programs OPAL, OpenH323 and PWLIB, and distribute the combination,
+ * without applying the requirements of the GNU GPL to the OPAL, OpenH323
+ * and PWLIB programs, as long as you do follow the requirements of the
+ * GNU GPL for all the rest of the software thus combined.
+ */
+
+
+/*
+ *                         ldap-book.cpp  -  description
+ *                         ------------------------------------------
+ *   begin                : written in 2007 by Julien Puydt
+ *   copyright            : (c) 2007 by Julien Puydt
+ *   description          : implementation of a LDAP book
+ *
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <ldap.h>
+#include <glib.h>
+
+#include "config.h"
+
+#include "ldap-book.h"
+#include "ekiga.h"
+#include "ui.h"
+#include "form-request-simple.h"
+
+
+/* little helper function... can probably be made more complete */
+static const std::string
+fix_to_utf8 (const std::string str)
+{
+  gchar *utf8_str = NULL;
+  std::string result;
+
+  if (g_utf8_validate (str.c_str (), -1, NULL))
+    utf8_str = g_strdup (str.c_str ());
+  else
+    utf8_str =  g_convert (str.c_str (), -1,
+                           "UTF-8", "ISO-8859-1",
+                           0, 0, 0);
+
+  result = std::string (utf8_str);
+
+  g_free (utf8_str);
+
+  return result;
+}
+
+/* parses a message to construct a nice contact */
+static OPENLDAP::Contact *
+parse_result (Ekiga::ServiceCore &core,
+	      struct ldap *context,
+	      LDAPMessage *message)
+{
+  OPENLDAP::Contact *result = NULL;
+  BerElement *ber = NULL;
+  char *attribute = NULL;
+  struct berval **values = NULL;
+  std::string name;
+  std::string call_address;
+
+  attribute = ldap_first_attribute (context, message, &ber);
+
+  while (attribute != NULL) {
+
+    if (strncmp("givenName",attribute, 9) == 0) {
+
+      values = ldap_get_values_len (context, message, attribute);
+      if (values[0] != NULL)
+	name = std::string (values[0]->bv_val, values[0]->bv_len);
+    }
+    if (strncmp ("telephoneNumber", attribute, 15) == 0) {
+
+      values = ldap_get_values_len (context, message, attribute);
+      if (values[0] != NULL)
+	call_address = std::string (values[0]->bv_val, values[0]->bv_len);
+    }
+    ldap_memfree (attribute);
+    attribute = ldap_next_attribute (context, message, ber);
+  }
+
+  if (!name.empty () && !call_address.empty ()) {
+
+    result = new OPENLDAP::Contact (core, fix_to_utf8 (name),
+				    /* FIXME: next line is annoying */
+				    std::string ("sip:") + call_address);
+  }
+
+  return result;
+}
+
+
+/* this allows us to do the refreshing in a thread: we put all needed
+ * data in this structure, let everything happen elsewhere, then push back
+ * into the main thread
+ */
+struct RefreshData
+{
+
+  RefreshData (Ekiga::ServiceCore &_core,
+	       const std::string _name,
+	       const std::string _hostname,
+	       int _port,
+	       const std::string _base,
+	       const std::string _scope,
+	       const std::string _call_attribute,
+	       const std::string _test,
+	       sigc::slot<void, std::vector<OPENLDAP::Contact *> > _publish_results):
+    core(_core), name(_name), hostname(_hostname), port(_port),
+    base(_base), scope(_scope), call_attribute(_call_attribute),
+    test(_test), publish_results (_publish_results)
+  {
+    error = false;
+  }
+
+  /* search data */
+  Ekiga::ServiceCore &core;
+  std::string name;
+  std::string hostname;
+  int port;
+  std::string base;
+  std::string scope;
+  std::string call_attribute;
+  std::string test;
+
+  /* result data */
+  bool error;
+  std::vector<OPENLDAP::Contact *> contacts;
+  std::string error_message;
+
+  /* callback */
+  sigc::slot<void, std::vector<OPENLDAP::Contact *> > publish_results;
+};
+
+/* actual implementation */
+
+OPENLDAP::Book::Book (Ekiga::ServiceCore &_core,
+		      xmlNodePtr _node): core(_core), node(_node)
+{
+  xmlChar *xml_str;
+
+  contact_core = dynamic_cast<Ekiga::ContactCore *>(core.get ("contact-core"));
+
+  for (xmlNodePtr child = node->children ;
+       child != NULL;
+       child = child->next) {
+
+    if (child->type == XML_ELEMENT_NODE
+	&& child->name != NULL) {
+
+      if (xmlStrEqual (BAD_CAST ("name"), child->name)) {
+
+	xml_str = xmlNodeGetContent (child);
+	name = (const char *)xml_str;
+	name_node = child;
+	xmlFree (xml_str);
+      }
+
+      if (xmlStrEqual (BAD_CAST ("hostname"), child->name)) {
+
+	xml_str = xmlNodeGetContent (child);
+	hostname = (const char *)xml_str;
+	hostname_node = child;
+	xmlFree (xml_str);
+      }
+
+      if (xmlStrEqual (BAD_CAST ("port"), child->name)) {
+
+	xml_str = xmlNodeGetContent (child);
+	port = std::atoi ((const char *)xml_str);
+	port_node = child;
+	xmlFree (xml_str);
+      }
+
+      if (xmlStrEqual (BAD_CAST ("base"), child->name)) {
+
+	xml_str = xmlNodeGetContent (child);
+	base = (const char *)xml_str;
+	base_node = child;
+	xmlFree (xml_str);
+      }
+
+      if (xmlStrEqual (BAD_CAST ("scope"), child->name)) {
+
+	xml_str = xmlNodeGetContent (child);
+	scope = (const char *)xml_str;
+	scope_node = child;
+	xmlFree (xml_str);
+      }
+
+      if (xmlStrEqual (BAD_CAST ("call_attribute"), child->name)) {
+
+	xml_str = xmlNodeGetContent (child);
+	call_attribute = (const char *)xml_str;
+	call_attribute_node = child;
+	xmlFree (xml_str);
+      }
+    }
+  }
+}
+
+OPENLDAP::Book::Book (Ekiga::ServiceCore &_core,
+		      const std::string _name,
+		      const std::string _hostname,
+		      int _port,
+		      const std::string _base,
+		      const std::string _scope,
+		      const std::string _call_attribute):
+  core(_core), name(_name), hostname(_hostname), port(_port),
+  base(_base), scope(_scope), call_attribute(_call_attribute),
+  test(""), ldap_context(NULL), patience(0)
+{
+  contact_core = dynamic_cast<Ekiga::ContactCore *>(core.get ("contact-core"));
+
+  node = xmlNewNode (NULL, BAD_CAST "server");
+
+  name_node = xmlNewChild (node, NULL,
+			   BAD_CAST "name", BAD_CAST name.c_str ());
+
+  hostname_node = xmlNewChild (node, NULL,
+			   BAD_CAST "hostname", BAD_CAST name.c_str ());
+
+  { // Snark hates C++ -- and there isn't only a reason for it -- but many
+    std::stringstream strm;
+    std::string port_string;
+    strm << port;
+    strm >> port_string;
+    hostname_node = xmlNewChild (node, NULL,
+				 BAD_CAST "port",
+				 BAD_CAST port_string.c_str ());
+  }
+
+  base_node = xmlNewChild (node, NULL,
+			   BAD_CAST "base", BAD_CAST name.c_str ());
+
+  scope_node = xmlNewChild (node, NULL,
+			    BAD_CAST "scope", BAD_CAST name.c_str ());
+
+  call_attribute_node = xmlNewChild (node, NULL,
+				     BAD_CAST "call_attribute",
+				     BAD_CAST name.c_str ());
+}
+
+OPENLDAP::Book::~Book ()
+{
+#ifdef __GNUC__
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+#endif
+}
+
+const std::string
+OPENLDAP::Book::get_name () const
+{
+  return name;
+}
+
+bool
+OPENLDAP::Book::populate_menu (Ekiga::MenuBuilder &builder)
+{
+  Ekiga::UI *ui = dynamic_cast<Ekiga::UI *>(core.get ("ui"));
+  builder.add_action ("refresh", _("Refresh"),
+		      sigc::mem_fun (this, &OPENLDAP::Book::refresh));
+  if (ui != NULL) {
+
+    builder.add_separator ();
+    builder.add_action ("edit", _("Edit"),
+			sigc::mem_fun (this, &OPENLDAP::Book::edit));
+  }
+
+  return true;
+}
+
+void
+OPENLDAP::Book::refresh ()
+{
+  if (ldap_context == NULL)
+    refresh_start ();
+}
+
+xmlNodePtr
+OPENLDAP::Book::get_node ()
+{
+  return node;
+}
+
+void
+OPENLDAP::Book::refresh_start ()
+{
+  int msgid = -1;
+  int result = LDAP_SUCCESS;
+  int ldap_version = LDAP_VERSION3;
+  std::vector<std::string> attributes_vector;
+  char **attributes = NULL;
+  std::string search_string;
+  int iscope = LDAP_SCOPE_SUBTREE;
+
+  attributes_vector.push_back ("givenname");
+  attributes_vector.push_back (call_attribute);
+
+  ldap_context = ldap_init (hostname.c_str (), port);
+  if (ldap_context == NULL) {
+
+    std::cout << "Couldn't open server " << hostname << std::endl; // FIXME: better error handling
+    return;
+  }
+
+  /* the openldap code shows I don't have to check the result of this
+   * (see for example tests/prog/slapd-search.c)
+   */
+  (void)ldap_set_option (ldap_context,
+			 LDAP_OPT_PROTOCOL_VERSION, &ldap_version);
+
+
+  result = ldap_bind_s (ldap_context, NULL, NULL, LDAP_AUTH_SIMPLE);
+  if (result != LDAP_SUCCESS) {
+
+    // FIXME: better error handling
+    std::cout << "Couldn't bind to " << hostname
+	      << ": " << ldap_err2string (result) << std::endl;
+    ldap_unbind (ldap_context);
+    ldap_context = NULL;
+    return;
+  }
+
+  if (scope == "sub")
+    iscope = LDAP_SCOPE_SUBTREE;
+  else
+    iscope = LDAP_SCOPE_ONELEVEL;
+
+  attributes = (char **)malloc ((attributes_vector.size ()+1)*sizeof(char *));
+  for (unsigned int i = 0; i < attributes_vector.size (); i++)
+    attributes[i] = strdup (attributes_vector[i].c_str ());
+  attributes[attributes_vector.size ()] = NULL;// don't ask how I remembered...
+
+  /* FIXME: http://bugzilla.gnome.org/show_bug.cgi?id=424459
+   * here things should be done with givenname=..., but for this bug
+   */
+  if (!test.empty ())
+    search_string = "(cn=*" + test + "*)";
+  else
+    search_string = "(cn=*)";
+
+  msgid = ldap_search (ldap_context,
+		       base.c_str (),
+		       iscope,
+		       search_string.c_str (),
+		       attributes,
+		       0); /* attrsonly */
+
+  for (unsigned int i = 0; i < attributes_vector.size (); i++)
+    free (attributes[i]);
+  free (attributes);
+
+  if (msgid == -1) {
+
+    std::cout << "couldn't start the search on " << hostname << std::endl; //FIXME: better error handling
+    ldap_unbind (ldap_context);
+    ldap_context = NULL;
+  }
+
+  patience = 3;
+  //FIXME
+  GnomeMeeting::Process ()->GetRuntime ()->run_later (sigc::mem_fun (this, &OPENLDAP::Book::refresh_end), 3);
+}
+
+void
+OPENLDAP::Book::refresh_end ()
+{
+  int result = LDAP_SUCCESS;
+  struct timeval timeout = { 1, 0}; /* block 1s */
+  LDAPMessage *msg_entry = NULL;
+  LDAPMessage *msg_result = NULL;
+  OPENLDAP::Contact *contact = NULL;
+
+  result = ldap_result (ldap_context, LDAP_RES_ANY, LDAP_MSG_ALL,
+			&timeout, &msg_entry);
+
+
+  if (result <= 0) {
+
+    if (patience == 3) {
+
+      patience--;
+      GnomeMeeting::Process ()->GetRuntime ()->run_later (sigc::mem_fun (this, &OPENLDAP::Book::refresh_end), 12);
+    } else if (patience == 2) {
+
+      patience--;
+      GnomeMeeting::Process ()->GetRuntime ()->run_later (sigc::mem_fun (this, &OPENLDAP::Book::refresh_end), 21);
+    } else if (patience == 1) {
+
+      patience--;
+      GnomeMeeting::Process ()->GetRuntime ()->run_later (sigc::mem_fun (this, &OPENLDAP::Book::refresh_end), 30);
+    } else { // patience == 0
+
+      std::cout << "couldn't search " << hostname
+		<< ": " << ((result == 0)?"timeout":"error") << std::endl; // FIXME: better error handling;
+
+      ldap_unbind (ldap_context);
+      ldap_context = NULL;
+    }
+
+    if (msg_entry != NULL)
+      ldap_msgfree (msg_entry);
+
+    return;
+  }
+
+  /* we flush */
+  iterator iter = begin ();
+  while (iter != end ()) {
+
+    remove_contact (*iter);
+    iter = begin ();
+  }
+
+  msg_result = ldap_first_message (ldap_context, msg_entry);
+  do {
+
+    if (ldap_msgtype (msg_result) == LDAP_RES_SEARCH_ENTRY) {
+
+      contact = parse_result (core, ldap_context, msg_result);
+      if (contact != NULL)
+	add_contact (*contact);
+    }
+    msg_result = ldap_next_message (ldap_context, msg_result);
+  } while (msg_result != NULL);
+
+  (void)ldap_msgfree (msg_entry);
+
+  ldap_unbind (ldap_context);
+  ldap_context = NULL;
+}
+
+void
+OPENLDAP::Book::edit ()
+{
+  Ekiga::FormRequestSimple request;
+  Ekiga::UI *ui = dynamic_cast<Ekiga::UI *>(core.get("ui"));
+
+  request.title (_("Edit LDAP directory"));
+
+  request.instructions (_("Please edit the following fields"));
+
+  request.text ("name", _("Name"), name);
+  request.text ("hostname", _("Hostname"), hostname);
+  { // Snark hates C++ -- and there isn't only a reason for it -- but many
+    std::stringstream strm;
+    std::string port_string;
+    strm << port;
+    strm >> port_string;
+    request.text ("port", _("Port"), port_string);
+  }
+  request.text ("base", _("Base DN"), base);
+
+  {
+    std::map<std::string, std::string> choices;
+
+    choices["sub"] = _("Subtree");
+    choices["single"] = _("Single level");
+    request.single_choice ("scope", _("Scope"), scope, choices);
+  }
+
+  request.text ("call-attribute", _("Call attribute"), call_attribute);
+
+  request.submitted.connect (sigc::mem_fun (this,
+					    &OPENLDAP::Book::on_edit_form_submitted));
+
+  ui->run_form_request (request);
+}
+
+void
+OPENLDAP::Book::on_edit_form_submitted (Ekiga::Form &result)
+{
+  try {
+
+    std::string new_name = result.text ("name");
+    std::string new_hostname = result.text ("hostname");
+    std::string new_port_string = result.text ("port");
+    std::string new_base = result.text ("base");
+    std::string new_scope = result.single_choice ("scope");
+    std::string new_call_attribute = result.text ("call-attribute");
+    int new_port = std::atoi (new_port_string.c_str ());
+
+    name = new_name;
+    xmlNodeSetContent (name_node,
+		       xmlEncodeSpecialChars(name_node->doc,
+					     BAD_CAST name.c_str ()));
+
+    hostname = new_hostname;
+    xmlNodeSetContent (hostname_node,
+		       xmlEncodeSpecialChars(hostname_node->doc,
+					     BAD_CAST hostname.c_str ()));
+
+    port = new_port;
+    xmlNodeSetContent (port_node,
+		       xmlEncodeSpecialChars(port_node->doc,
+					     BAD_CAST new_port_string.c_str ()));
+    base = new_base;
+    xmlNodeSetContent (base_node,
+		       xmlEncodeSpecialChars(base_node->doc,
+					     BAD_CAST base.c_str ()));
+    scope = new_scope;
+    xmlNodeSetContent (scope_node,
+		       xmlEncodeSpecialChars(scope_node->doc,
+					     BAD_CAST scope.c_str ()));
+    call_attribute = new_call_attribute;
+    xmlNodeSetContent (call_attribute_node,
+		       xmlEncodeSpecialChars(call_attribute_node->doc,
+					     BAD_CAST call_attribute.c_str ()));
+
+    updated.emit ();
+    trigger_saving.emit ();
+
+  } catch (Ekiga::Form::not_found) {
+
+    std::cerr << "Invalid result form" << std::endl; // FIXME: do better
+  }
+}
---
+++ lib/engine/addressbook/ldap/ldap-source.cpp
@@ -0,0 +1,202 @@
+
+/* Ekiga -- A VoIP and Video-Conferencing application
+ * Copyright (C) 2000-2007 Damien Sandras
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ * Ekiga is licensed under the GPL license and as a special exception,
+ * you have permission to link or otherwise combine this program with the
+ * programs OPAL, OpenH323 and PWLIB, and distribute the combination,
+ * without applying the requirements of the GNU GPL to the OPAL, OpenH323
+ * and PWLIB programs, as long as you do follow the requirements of the
+ * GNU GPL for all the rest of the software thus combined.
+ */
+
+
+/*
+ *                         ldap-source.cpp  -  description
+ *                         ------------------------------------------
+ *   begin                : written in 2007 by Julien Puydt
+ *   copyright            : (c) 2007 by Julien Puydt
+ *   description          : implementation of a LDAP source
+ *
+ */
+
+#include <iostream>
+
+#include "config.h"
+
+#include "gmconf.h"
+#include "form-request-simple.h"
+#include "ui.h"
+
+#include "ldap-source.h"
+
+#define KEY "/apps/" PACKAGE_NAME "/contacts/ldap_servers"
+
+OPENLDAP::Source::Source (Ekiga::ServiceCore &_core): core(_core), doc(NULL)
+{
+  xmlNodePtr root;
+  const gchar *c_raw = gm_conf_get_string (KEY);
+
+  if (c_raw != NULL) {
+
+    const std::string raw = c_raw;
+
+    doc = xmlRecoverMemory (raw.c_str (), raw.length ());
+
+    root = xmlDocGetRootElement (doc);
+
+    if (root == NULL) {
+
+      root = xmlNewNode (NULL, BAD_CAST "list");
+      xmlDocSetRootElement (doc, root);
+    }
+
+    for (xmlNodePtr child = root->children ;
+	 child != NULL ;
+	 child = child->next)
+      if (child->type == XML_ELEMENT_NODE
+	  && child->name != NULL
+	  && xmlStrEqual (BAD_CAST "server", child->name))
+	add (child);
+  } else
+    doc = xmlNewDoc (BAD_CAST "1.0");
+}
+
+void
+OPENLDAP::Source::add (xmlNodePtr node)
+{
+  Book *book = NULL;
+
+  book = new Book (core, node);
+
+  common_add (*book);
+}
+
+void
+OPENLDAP::Source::add (const std::string name,
+		       const std::string hostname,
+		       int port,
+		       const std::string base,
+		       const std::string scope,
+		       const std::string call_attribute)
+{
+  Book *book = NULL;
+  xmlNodePtr root;
+
+  root = xmlDocGetRootElement (doc);
+  book = new Book (core, name, hostname, port, base, scope, call_attribute);
+
+  xmlAddChild (root, book->get_node ());
+
+  common_add (*book);
+}
+
+void
+OPENLDAP::Source::common_add (Book &book)
+{
+  book.trigger_saving.connect (sigc::mem_fun (this,
+					      &OPENLDAP::Source::save));
+  add_book (book);
+}
+
+OPENLDAP::Source::~Source ()
+{
+#ifdef __GNUC__
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+#endif
+}
+
+bool
+OPENLDAP::Source::populate_menu (Ekiga::MenuBuilder &builder)
+{
+  Ekiga::UI *ui = dynamic_cast<Ekiga::UI*>(core.get ("ui"));
+
+  if (ui != NULL) {
+
+    builder.add_action ("add", _("Add an LDAP addressbook"),
+			sigc::mem_fun (this,
+				       &OPENLDAP::Source::new_book));
+    return true;
+  } else
+    return false;
+}
+
+void
+OPENLDAP::Source::new_book ()
+{
+  Ekiga::UI *ui = dynamic_cast<Ekiga::UI*>(core.get ("ui"));
+  Ekiga::FormRequestSimple request;
+
+  request.title (_("Create LDAP directory"));
+
+  request.instructions (_("Please edit the following fields"));
+
+  request.text ("name", _("Name"), "");
+  request.text ("hostname", _("Hostname"), "");
+  request.text ("port", _("Port"), "389");
+  request.text ("base", _("Base DN"), "dc=net");
+  {
+    std::map<std::string, std::string> choices;
+
+    choices["sub"] = _("Subtree");
+    choices["single"] = _("Single level");
+    request.single_choice ("scope", _("Scope"), "sub", choices);
+  }
+
+  request.text ("call-attribute", _("Call attribute"), "telephoneNumber");
+
+  request.submitted.connect (sigc::mem_fun (this,
+					    &OPENLDAP::Source::on_new_book_form_submitted));
+
+  ui->run_form_request (request);
+}
+
+void
+OPENLDAP::Source::on_new_book_form_submitted (Ekiga::Form &result)
+{
+  try {
+
+    std::string name = result.text ("name");
+    std::string hostname = result.text ("hostname");
+    std::string port_string = result.text ("port");
+    std::string base = result.text ("base");
+    std::string scope = result.single_choice ("scope");
+    std::string call_attribute = result.text ("call-attribute");
+    int port = std::atoi (port_string.c_str ());
+
+    add (name, hostname, port, base, scope, call_attribute);
+    save ();
+
+  } catch (Ekiga::Form::not_found) {
+
+    std::cerr << "Invalid result form" << std::endl; // FIXME: do better
+  }
+}
+
+void
+OPENLDAP::Source::save ()
+{
+  xmlChar *buffer = NULL;
+  int size = 0;
+
+  xmlDocDumpMemory (doc, &buffer, &size);
+
+  gm_conf_set_string (KEY, (const char *)buffer);
+
+  xmlFree (buffer);
+}
