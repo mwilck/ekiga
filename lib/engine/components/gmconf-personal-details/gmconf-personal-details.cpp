3f69b56473d856d6a312c3e6a7531433b621a846
Damien Sandras <dsandras@src.gnome.org>
2008-01-19 22:04:50 +0000

    Added new framework to manage personal details (display name, status, ...) and a GmConf implementation. When one detail change, interested objects can react appropriately if they have connected to the right signal.
    svn path=/trunk/; revision=5921

---
+++ lib/engine/components/gmconf-personal-details/gmconf-personal-details.cpp
@@ -0,0 +1,90 @@
+
+/*
+ * Ekiga -- A VoIP and Video-Conferencing application
+ * Copyright (C) 2000-2008 Damien Sandras
+
+ * This program is free software; you can  redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version. This program is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Ekiga is licensed under the GPL license and as a special exception, you
+ * have permission to link or otherwise combine this program with the
+ * programs OPAL, OpenH323 and PWLIB, and distribute the combination, without
+ * applying the requirements of the GNU GPL to the OPAL, OpenH323 and PWLIB
+ * programs, as long as you do follow the requirements of the GNU GPL for all
+ * the rest of the software thus combined.
+ */
+
+
+/*
+ *                         gmconf-personal-details.h  -  description
+ *                         ------------------------------------------
+ *   begin                : written in 2008 by Damien Sandras
+ *   copyright            : (c) 2008 by Damien Sandras
+ *   description          : declaration of the representation of personal details
+ *                          using gmconf
+ *
+ */
+
+#include <glib.h>
+
+#include "gmconf.h"
+#include "gmconf-personal-details.h"
+
+#include <iostream>
+
+using namespace Gmconf;
+
+static void
+display_name_changed_nt (gpointer /*id*/,
+                         GmConfEntry *entry,
+                         gpointer data)
+{
+  PersonalDetails *details = (PersonalDetails *) data;
+
+  if (gm_conf_entry_get_type (entry) == GM_CONF_STRING) 
+    details->set_display_name (gm_conf_entry_get_string (entry));
+  
+}
+
+static void
+short_status_changed_nt (gpointer /*id*/,
+                         GmConfEntry *entry,
+                         gpointer data)
+{
+  PersonalDetails *details = (PersonalDetails *) data;
+
+  if (gm_conf_entry_get_type (entry) == GM_CONF_STRING) 
+    details->set_short_status (gm_conf_entry_get_string (entry));
+  
+}
+
+static void
+long_status_changed_nt (gpointer /*id*/,
+                        GmConfEntry *entry,
+                        gpointer data)
+{
+  PersonalDetails *details = (PersonalDetails *) data;
+
+  if (gm_conf_entry_get_type (entry) == GM_CONF_STRING) 
+    details->set_long_status (gm_conf_entry_get_string (entry));
+}
+
+PersonalDetails::PersonalDetails () 
+{
+  gm_conf_notifier_add ("/apps/ekiga/general/personal_data/full_name",
+                        display_name_changed_nt, this);
+  gm_conf_notifier_add ("/apps/ekiga/general/personal_data/short_status",
+                        short_status_changed_nt, this);
+  gm_conf_notifier_add ("/apps/ekiga/general/personal_data/long_status",
+                        long_status_changed_nt, this);
+}
+
