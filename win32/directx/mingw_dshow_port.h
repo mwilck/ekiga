fb19044aedb2c8def901a99de72cad2ed2a2d51c
Matthias Schneider <mschneid@src.gnome.org>
2008-09-28 09:29:29 +0000

    Fix debug build for win32.
    svn path=/branches/gnome-2-24/; revision=7113

--- win32/directx/mingw_dshow_port.h
+++ win32/directx/mingw_dshow_port.h
@@ -11,4 +11,4 @@
 #undef _X86_
 #define _I64_MAX LONG_LONG_MAX
 #define EXTERN_GUID(itf,l1,s1,s2,c1,c2,c3,c4,c5,c6,c7,c8)  \
-	EXTERN_C static const IID itf = {l1,s1,s2,{c1,c2,c3,c4,c5,c6,c7,c8} }
+	extern "C" { static const IID itf = {l1,s1,s2,{c1,c2,c3,c4,c5,c6,c7,c8} }; }
