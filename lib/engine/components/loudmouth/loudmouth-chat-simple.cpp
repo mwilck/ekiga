cad196ba6d964fe4053ab328a4f428869c09e3b9
Julien Puydt <jpuydt@noether.localdomain>
2008-12-03 18:19:47 +0100

    Added a simple chat implementation -- it compiles but isn't hooked

---
+++ lib/engine/components/loudmouth/loudmouth-chat-simple.cpp
@@ -0,0 +1,107 @@
+
+/*
+ * Ekiga -- A VoIP and Video-Conferencing application
+ * Copyright (C) 2000-2008 Damien Sandras
+
+ * This program is free software; you can  redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version. This program is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Ekiga is licensed under the GPL license and as a special exception, you
+ * have permission to link or otherwise combine this program with the
+ * programs OPAL, OpenH323 and PWLIB, and distribute the combination, without
+ * applying the requirements of the GNU GPL to the OPAL, OpenH323 and PWLIB
+ * programs, as long as you do follow the requirements of the GNU GPL for all
+ * the rest of the software thus combined.
+ */
+
+
+/*
+ *                         loudmouth-chat-simple.cpp  -  description
+ *                         ------------------------------------------
+ *   begin                : written in 2008 by Julien Puydt
+ *   copyright            : (c) 2008 by Julien Puydt
+ *   description          : implementation of a loudmouth simple chat
+ *
+ */
+
+#include "loudmouth-chat-simple.h"
+
+LM::SimpleChat::SimpleChat (LmConnection* connection_,
+			    gmref_ptr<Presentity> presentity_):
+  connection(connection_), presentity(presentity_)
+{
+  lm_connection_ref (connection);
+  presentity->has_chat = true;
+}
+
+LM::SimpleChat::~SimpleChat ()
+{
+  lm_connection_unref (connection);
+  presentity->has_chat = false;
+}
+
+const std::string
+LM::SimpleChat::get_title () const
+{
+  return presentity->get_name ();
+}
+
+void
+LM::SimpleChat::connect (gmref_ptr<Ekiga::ChatObserver> observer)
+{
+  observers.push_back (observer);
+}
+
+void
+LM::SimpleChat::disconnect (gmref_ptr<Ekiga::ChatObserver> observer)
+{
+  observers.remove (observer);
+}
+
+bool
+LM::SimpleChat::send_message (const std::string msg)
+{
+  bool result = false;
+
+  if (lm_connection_is_authenticated (connection)) {
+
+    result = true;
+    LmMessage* message = lm_message_new (NULL, LM_MESSAGE_TYPE_MESSAGE);
+    lm_message_node_set_attributes (lm_message_get_node (message),
+				    "to", presentity->get_jid ().c_str (),
+				    "type", "chat",
+				    NULL);
+    lm_message_node_add_child (lm_message_get_node (message), "body", msg.c_str ());
+    lm_connection_send (connection, message, NULL);
+    lm_message_unref (message);
+    for (std::list<gmref_ptr<Ekiga::ChatObserver> >::iterator iter = observers.begin ();
+	 iter != observers.end ();
+	 ++iter) {
+
+      (*iter)->message (presentity->get_name (), msg);
+    }
+  }
+
+  return result;
+}
+
+bool
+LM::SimpleChat::populate_menu (Ekiga::MenuBuilder& /*builder*/)
+{
+  return false; // FIXME;
+}
+
+gmref_ptr<Ekiga::Presentity>
+LM::SimpleChat::get_presentity () const
+{
+  return presentity;
+}
