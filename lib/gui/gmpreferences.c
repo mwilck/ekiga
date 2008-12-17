bf24019f79e15fbc95f2c0e0dfc4c489c80491a0
Damien Sandras <dsandras@src.gnome.org>
2008-12-17 19:27:54 +0000

    Removed deprecated stuff.
    svn path=/trunk/; revision=7466

--- lib/gui/gmpreferences.c
+++ lib/gui/gmpreferences.c
@@ -368,7 +368,7 @@ gnome_prefs_spin_new (GtkWidget *table,
   adj = (GtkAdjustment *) 
     gtk_adjustment_new (gm_conf_get_int (conf_key),
 			min, max, step,
-			10.0, 1.0);
+			10.0, 0.0);
   
   spin_button = gtk_spin_button_new (adj, 1.0, 0);
   if (!writable)
