34d587b8f04e471539d5d6aa4c239b5861816227
Julien Puydt <jpuydt@src.gnome.org>
2009-02-06 09:08:41 +0000

    Applied patch from Thomas Andersen, fixing bug #560285. Thanks!
    svn path=/trunk/; revision=7644

--- lib/platform/platform-win32.c
+++ lib/platform/platform-win32.c
@@ -49,7 +49,11 @@ static gchar *plugindir = NULL;
 void
 gm_platform_init ()
 {
+#if GLIB_CHECK_VERSION (2, 18, 0)
+  basedir = g_strdup (g_win32_get_package_installation_directory_of_module (NULL, NULL));
+#else  
   basedir = g_strdup (g_win32_get_package_installation_directory (NULL, NULL));
+#endif
   sysconfdir = g_strdup (basedir);
   datadir = g_strdup (basedir);
   plugindir = g_strdup_printf ("%s/lib/contacts", basedir);
