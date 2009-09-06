0ea0c79836b9069f01c133801ac41d06dde4f099
Julien Puydt <jpuydt@gnome.org>
2009-09-06 14:18:00 +0200

    Added a boost::throw_exception function
    This is as Michael Rickmann suggested.

---
+++ lib/engine/framework/boost-exceptions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <boost/throw_exception.hpp>
+
+#ifdef BOOST_NO_EXCEPTIONS
+void
+boost::throw_exception (const std::exception&)
+{
+  std::cerr << "Unhandled exception" << std::endl; // FIXME: do better
+  abort ();
+}
+#endif
