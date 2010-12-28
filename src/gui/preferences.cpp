mwilck/ekiga

commit 28f431b4f7f2ed537d8ef888614110c716630406

Hotplug notifications support thanks to Mr M.
svn path=/trunk/; revision=6572

src/gui/preferences.cpp
@@ -1258,16 +1258,16 @@ audioev_filename_browse_play_cb (GtkWidget* /* playbutton */,
    g_free (file_name);
 }
 
-void on_videoinput_device_added_cb (const Ekiga::VideoInputDevice & device, GtkWidget *prefs_window)
+void on_videoinput_device_added_cb (const Ekiga::VideoInputDevice & device, bool isDesired, GtkWidget *prefs_window)
 {
   GmPreferencesWindow *pw = NULL;
   g_return_if_fail (prefs_window != NULL);
   pw = gm_pw_get_pw (prefs_window);
   std::string device_string = device.type + "/" + device.source + "/" + device.name;
-  gnome_prefs_string_option_menu_add (pw->video_device, device_string.c_str());
+  gnome_prefs_string_option_menu_add (pw->video_device, device_string.c_str(),  isDesired ? TRUE : FALSE);
 }
 
-void on_videoinput_device_removed_cb (const Ekiga::VideoInputDevice & device, GtkWidget *prefs_window)
+void on_videoinput_device_removed_cb (const Ekiga::VideoInputDevice & device, bool, GtkWidget *prefs_window)
 {
   GmPreferencesWindow *pw = NULL;
   g_return_if_fail (prefs_window != NULL);
@@ -1276,17 +1276,17 @@ void on_videoinput_device_removed_cb (const Ekiga::VideoInputDevice & device, Gt
   gnome_prefs_string_option_menu_remove(pw->video_device, device_string.c_str());
 }
 
-void on_audioinput_device_added_cb (const Ekiga::AudioInputDevice & audioinput_device, GtkWidget *prefs_window)
+void on_audioinput_device_added_cb (const Ekiga::AudioInputDevice & audioinput_device, bool isDesired, GtkWidget *prefs_window)
 {
   GmPreferencesWindow *pw = NULL;
   g_return_if_fail (prefs_window != NULL);
   pw = gm_pw_get_pw (prefs_window);
   std::string device = audioinput_device.type + "/" + audioinput_device.source + "/" + audioinput_device.name;
-  gnome_prefs_string_option_menu_add (pw->audio_recorder, device.c_str());
+  gnome_prefs_string_option_menu_add (pw->audio_recorder, device.c_str(),  isDesired ? TRUE : FALSE);
 
 }
 
-void on_audioinput_device_removed_cb (const Ekiga::AudioInputDevice & audioinput_device, GtkWidget *prefs_window)
+void on_audioinput_device_removed_cb (const Ekiga::AudioInputDevice & audioinput_device, bool, GtkWidget *prefs_window)
 {
   GmPreferencesWindow *pw = NULL;
   g_return_if_fail (prefs_window != NULL);
@@ -1295,17 +1295,17 @@ void on_audioinput_device_removed_cb (const Ekiga::AudioInputDevice & audioinput
   gnome_prefs_string_option_menu_remove(pw->audio_recorder, device.c_str());
 }
 
-void on_audiooutput_device_added_cb (const Ekiga::AudioOutputDevice & audiooutput_device, GtkWidget *prefs_window)
+void on_audiooutput_device_added_cb (const Ekiga::AudioOutputDevice & audiooutput_device, bool isDesired,  GtkWidget *prefs_window)
 {
   GmPreferencesWindow *pw = NULL;
   g_return_if_fail (prefs_window != NULL);
   pw = gm_pw_get_pw (prefs_window);
   std::string device = audiooutput_device.type + "/" + audiooutput_device.source + "/" + audiooutput_device.name;
-  gnome_prefs_string_option_menu_add (pw->audio_player, device.c_str());
-  gnome_prefs_string_option_menu_add (pw->sound_events_output, device.c_str());
+  gnome_prefs_string_option_menu_add (pw->audio_player, device.c_str(), isDesired ? TRUE : FALSE);
+  gnome_prefs_string_option_menu_add (pw->sound_events_output, device.c_str(), FALSE);
 }
 
-void on_audiooutput_device_removed_cb (const Ekiga::AudioOutputDevice & audiooutput_device, GtkWidget *prefs_window)
+void on_audiooutput_device_removed_cb (const Ekiga::AudioOutputDevice & audiooutput_device, bool, GtkWidget *prefs_window)
 {
   GmPreferencesWindow *pw = NULL;
   g_return_if_fail (prefs_window != NULL);

lib/engine/audioinput/skel/audioinput-core.cpp
@@ -220,7 +220,7 @@ void AudioInputCore::set_device(const AudioInputDevice & device)
 
 void AudioInputCore::add_device (const std::string & source, const std::string & device_name, HalManager* /*manager*/)
 {
-  PTRACE(0, "AudioInputCore\tAdding Device " << device_name);
+  PTRACE(4, "AudioInputCore\tAdding Device " << device_name);
   yield = true;
   PWaitAndSignal m(core_mutex);
 
@@ -233,14 +233,14 @@ void AudioInputCore::add_device (const std::string & source, const std::string &
        if ( desired_device == device)
          internal_set_device(desired_device);
 
-       device_added.emit (device);
+       device_added.emit (device, desired_device == device);
      }
   }
 }
 
 void AudioInputCore::remove_device (const std::string & source, const std::string & device_name, HalManager* /*manager*/)
 {
-  PTRACE(0, "AudioInputCore\tRemoving Device " << device_name);
+  PTRACE(4, "AudioInputCore\tRemoving Device " << device_name);
   yield = true;
   PWaitAndSignal m(core_mutex);
 
@@ -258,7 +258,7 @@ void AudioInputCore::remove_device (const std::string & source, const std::strin
             new_device.name = AUDIO_INPUT_FALLBACK_DEVICE_NAME;
             internal_set_device( new_device);
        }
-       device_removed.emit (device);
+       device_removed.emit (device,  current_device == device);
      }
   }
 }

lib/engine/audioinput/skel/audioinput-core.h
@@ -271,14 +271,14 @@ namespace Ekiga
        * a manager claimed support for this device.
        * @param device the audio input device that was added.
        */
-      sigc::signal<void, AudioInputDevice> device_added;
+      sigc::signal<void, AudioInputDevice, bool> device_added;
 
       /** This signal is emitted when an audio input device has been removed from the system.
        * This signal will be emitted if remove_device was called with a device name and
        * a manager claimed support for this device.
        * @param device the audio input device that was removed.
        */
-      sigc::signal<void, AudioInputDevice> device_removed;
+      sigc::signal<void, AudioInputDevice, bool> device_removed;
 
   private:
       void on_device_opened (AudioInputDevice device,  

lib/engine/audiooutput/skel/audiooutput-core.cpp
@@ -195,7 +195,7 @@ void AudioOutputCore::set_device(AudioOutputPS ps, const AudioOutputDevice & dev
 
 void AudioOutputCore::add_device (const std::string & sink, const std::string & device_name, HalManager* /*manager*/)
 {
-  PTRACE(0, "AudioOutputCore\tAdding Device " << device_name);
+  PTRACE(4, "AudioOutputCore\tAdding Device " << device_name);
   yield = true;
   PWaitAndSignal m_pri(core_mutex[primary]);
 
@@ -209,14 +209,14 @@ void AudioOutputCore::add_device (const std::string & sink, const std::string & 
          internal_set_primary_device(desired_primary_device);
        }
 
-       device_added.emit(device);
+       device_added.emit(device, desired_primary_device == device);
      }
   }
 }
 
 void AudioOutputCore::remove_device (const std::string & sink, const std::string & device_name, HalManager* /*manager*/)
 {
-  PTRACE(0, "AudioOutputCore\tRemoving Device " << device_name);
+  PTRACE(4, "AudioOutputCore\tRemoving Device " << device_name);
   yield = true;
   PWaitAndSignal m_pri(core_mutex[primary]);
 
@@ -234,7 +234,7 @@ void AudioOutputCore::remove_device (const std::string & sink, const std::string
          internal_set_primary_device(new_device);
        }
 
-       device_removed.emit(device);
+       device_removed.emit(device, device == current_device[primary]);
      }
   }
 }

lib/engine/audiooutput/skel/audiooutput-core.h
@@ -315,14 +315,14 @@ namespace Ekiga
        * a manager claimed support for this device.
        * @param device the audio output device that was added.
        */
-      sigc::signal<void, AudioOutputDevice> device_added;
+      sigc::signal<void, AudioOutputDevice, bool> device_added;
 
       /** This signal is emitted when an audio output device has been removed from the system.
        * This signal will be emitted if remove_device was called with a device name and
        * a manager claimed support for this device.
        * @param device the audio output device that was removed.
        */
-      sigc::signal<void, AudioOutputDevice> device_removed;
+      sigc::signal<void, AudioOutputDevice, bool> device_removed;
 
   private:
       void on_device_opened (AudioOutputPS ps, 

lib/engine/videoinput/skel/videoinput-core.cpp
@@ -215,7 +215,7 @@ void VideoInputCore::set_device(const VideoInputDevice & device, int channel, Vi
 
 void VideoInputCore::add_device (const std::string & source, const std::string & device_name, unsigned capabilities, HalManager* /*manager*/)
 {
-  PTRACE(0, "VidInputCore\tAdding Device " << device_name);
+  PTRACE(4, "VidInputCore\tAdding Device " << device_name);
   PWaitAndSignal m(core_mutex);
 
   VideoInputDevice device;
@@ -227,14 +227,14 @@ void VideoInputCore::add_device (const std::string & source, const std::string &
       if ( desired_device == device )
         internal_set_device(device, current_channel, current_format);
 
-      device_added.emit(device);
+      device_added.emit(device, desired_device == device);
     }
   }
 }
 
 void VideoInputCore::remove_device (const std::string & source, const std::string & device_name, unsigned capabilities, HalManager* /*manager*/)
 {
-  PTRACE(0, "VidInputCore\tRemoving Device " << device_name);
+  PTRACE(4, "VidInputCore\tRemoving Device " << device_name);
   PWaitAndSignal m(core_mutex);
 
   VideoInputDevice device;
@@ -251,7 +251,7 @@ void VideoInputCore::remove_device (const std::string & source, const std::strin
             internal_set_device(new_device, current_channel, current_format);
        }
 
-       device_removed.emit(device);
+       device_removed.emit(device, current_device == device);
      }
   }
 }

lib/engine/videoinput/skel/videoinput-core.h
@@ -290,14 +290,14 @@ namespace Ekiga
        * a manager claimed support for this device.
        * @param device the video input device that was added.
        */
-      sigc::signal<void, VideoInputDevice> device_added;
+      sigc::signal<void, VideoInputDevice, bool> device_added;
 
       /** This signal is emitted when a video input has been removed from the system.
        * This signal will be emitted if remove_device was called with a device name and
        * a manager claimed support for this device.
        * @param device the video input device that was removed.
        */
-      sigc::signal<void, VideoInputDevice> device_removed;
+      sigc::signal<void, VideoInputDevice, bool> device_removed;
 
   private:
       void on_device_opened (VideoInputDevice device,  

lib/gui/gmpreferences.c
@@ -757,7 +757,8 @@ gnome_prefs_string_option_menu_update (GtkWidget *option_menu,
 
 void
 gnome_prefs_string_option_menu_add (GtkWidget *option_menu,
-				    const gchar *option)
+				    const gchar *option,
+				    gboolean active)
 {
   GtkTreeModel *model = NULL;
   GtkTreeIter iter;
@@ -772,7 +773,8 @@ gnome_prefs_string_option_menu_add (GtkWidget *option_menu,
   gtk_list_store_set (GTK_LIST_STORE (model), &iter, 
                              COLUMN_STRING_RAW, option,
                             COLUMN_STRING_TRANSLATED, option_string, -1);
-
+  if (active == TRUE)
+    gtk_combo_box_set_active_iter (GTK_COMBO_BOX (option_menu), &iter);
   g_free (option_string);
 }
 

commit bc462f016ed4f726845a7ca69c53b8efe12acc00

Select ringing device too when pluging an audio output device
Fixes partially bug #620795.  This bug was introduced by commit
28f431b4.

src/gui/preferences.cpp
@@ -1258,7 +1258,7 @@ void on_audiooutput_device_added_cb (const Ekiga::AudioOutputDevice & device, bo
   g_return_if_fail (prefs_window != NULL);
   pw = gm_pw_get_pw (prefs_window);
   gnome_prefs_string_option_menu_add (pw->audio_player, (device.GetString()).c_str(), isDesired ? TRUE : FALSE);
-  gnome_prefs_string_option_menu_add (pw->sound_events_output, (device.GetString()).c_str(), FALSE);
+  gnome_prefs_string_option_menu_add (pw->sound_events_output, (device.GetString()).c_str(), isDesired ? TRUE : FALSE);
 }
 
 void on_audiooutput_device_removed_cb (const Ekiga::AudioOutputDevice & device, bool, GtkWidget *prefs_window)