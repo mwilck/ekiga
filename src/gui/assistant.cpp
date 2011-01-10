cb04163c77436160f776181d026f0689a4797ac2
Eugen Dedu <Eugen.Dedu@pu-pm.univ-fcomte.fr>
2011-01-10 20:10:47 +0100

    Remove two endlines to reduce assistant window size
    Fixes bug #580533.  A general solution for even smaller screens is
    still needed.

--- src/gui/assistant.cpp
+++ src/gui/assistant.cpp
@@ -705,7 +705,7 @@ create_ekiga_out_page (EkigaAssistant *assistant)
 
   label = gtk_label_new (NULL);
   text = g_strdup_printf ("<i>%s</i>",
-                          _("You can make calls to regular phones and cell numbers worldwide using Ekiga.\n\n"
+                          _("You can make calls to regular phones and cell numbers worldwide using Ekiga. "
                             "To enable this, you need to do two things:\n"
                             "- First buy an account at the URL below.\n"
                             "- Then enter your account ID and PIN code.\n"
