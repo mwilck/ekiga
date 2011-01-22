7cc3e6e2e14632d020f138f3bda1e95dfc5251c1
Eugen Dedu <Eugen.Dedu@pu-pm.univ-fcomte.fr>
2011-01-22 16:28:41 +0100

    Revert "Removed the obsolete OnMessageFailed method in the SIP endpoint code"
    This reverts commit e5afd686458c3390356393ee546471baab18b67b.  This
    method is used when there is an error when sending a message in a chat.

--- lib/engine/components/opal/sip-endpoint.h
+++ lib/engine/components/opal/sip-endpoint.h
@@ -169,6 +169,9 @@ namespace Opal {
       bool OnReceivedMESSAGE (OpalTransport & transport,
                               SIP_PDU & pdu);
 
+      void OnMessageFailed (const SIPURL & messageUrl,
+                            SIP_PDU::StatusCodes reason);
+
       SIPURL GetRegisteredPartyName (const SIPURL & host,
 				     const OpalTransport & transport);
 
