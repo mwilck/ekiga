931760ec0566e75e133615e0de5532a9202377bf
Eugen Dedu <Eugen.Dedu@pu-pm.univ-fcomte.fr>
2013-01-25 20:46:55 +0100

    Remove some unused #include

--- lib/engine/components/call-history/history-book.cpp
+++ lib/engine/components/call-history/history-book.cpp
@@ -33,16 +33,9 @@
  *
  */
 
-#include <iostream>
-#include <sstream>
-#include <set>
-
 #include "config.h"
-
 #include <glib/gi18n.h>
-
 #include "gmconf.h"
-
 #include "history-book.h"
 
 History::Book::Book (Ekiga::ServiceCore& core):
