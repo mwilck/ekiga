208c5deaad5b32594b552dd5b9cf129cf52fb789
Damien Sandras <dsandras@beip.be>
2013-04-13 19:17:36 +0200

    GmPrefs: Fixed sound event playing if full filename is provided.
    This probably also affects 4.0.x.

--- lib/engine/gui/gtk-frontend/preferences-window.cpp
+++ lib/engine/gui/gtk-frontend/preferences-window.cpp
@@ -1112,7 +1112,10 @@ sound_event_play_cb (G_GNUC_UNUSED GtkWidget *widget,
     boost::shared_ptr<Ekiga::AudioOutputCore> audiooutput_core = pw->core->get<Ekiga::AudioOutputCore> ("audiooutput-core");
 
     if (sound_event) {
-      audiooutput_core->play_event(sound_event);
+      if (!g_path_is_absolute (sound_event))
+        audiooutput_core->play_event(sound_event);
+      else
+        audiooutput_core->play_file(sound_event);
       g_free (sound_event);
     }
   }
