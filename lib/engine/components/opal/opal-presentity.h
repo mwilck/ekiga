051a4e9dcdbecc59d13f89e43789ba53f88123c7
Julien Puydt <jpuydt@free.fr>
2013-10-25 15:27:44 +0200

    Added an experimental Opal::Presentity implementation
    This is heavily based on Local::Presentity's code, but with some little
    improvements here and there (pushing build_node as a static method makes
    a better api, for instance), removed presence fetching/unfetching code
    (which will need to be re-added later, perhaps differently).
    
    It hasn't been added to the build system, and was never compiled.

---
+++ lib/engine/components/opal/opal-presentity.h
@@ -0,0 +1,142 @@
+
+/*
+ * Ekiga -- A VoIP and Video-Conferencing application
+ * Copyright (C) 2000-2013 Damien Sandras <dsandras@seconix.com>
+
+ * This program is free software; you can  redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version. This program is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Ekiga is licensed under the GPL license and as a special exception, you
+ * have permission to link or otherwise combine this program with the
+ * programs OPAL, OpenH323 and PWLIB, and distribute the combination, without
+ * applying the requirements of the GNU GPL to the OPAL, OpenH323 and PWLIB
+ * programs, as long as you do follow the requirements of the GNU GPL for all
+ * the rest of the software thus combined.
+ */
+
+
+/*
+ *                         opal-presentity.h  -  description
+ *                         ------------------------------------------
+ *   begin                : written in 2013 by Julien Puydt
+ *   copyright            : (c) 2013 by Julien Puydt
+ *   description          : declaration of a presentity for an opal account roster
+ *
+ */
+
+
+
+#ifndef __OPAL_PRESENTITY_H__
+#define __OPAL_PRESENTITY_H__
+
+#include <libxml/tree.h>
+#include <boost/smart_ptr.hpp>
+
+#include "form.h"
+#include "presence-core.h"
+#include "presentity.h"
+
+namespace Opal
+{
+  class Cluster;
+
+  /* This class implements and Ekiga::Presentity, stored as a node
+   * in an XML document -- the code is relative to that node, so could
+   * probably be abstracted, should the need arise!
+   */
+
+  class Presentity: public Ekiga::Presentity
+  {
+  public:
+
+    /* build a node describing a valid presentity, which the caller
+     * will then use to create a valid instance using the ctor */
+    static xmlNodePtr build_node (const std::string name_,
+				  const std::string uri_,
+				  const std::set<std::string> groups_);
+
+    Presentity (boost::weak_ptr<Opal::Cluster> cluster_,
+		boost::weak_ptr<Ekiga::PresenceCore> presence_core_,
+		boost::shared_ptr<xmlDoc> doc_,
+		xmlNodePtr node_);
+
+    ~Presentity ();
+
+    /* methods of the general Ekiga::Presentity class: */
+
+    const std::string get_name () const;
+
+    const std::string get_presence () const;
+
+    const std::string get_status () const;
+
+    const std::set<std::string> get_groups () const;
+
+    const std::string get_uri () const;
+
+    bool has_uri (const std::string uri) const;
+
+    bool populate_menu (Ekiga::MenuBuilder &);
+
+    /* setter methods specific for this class of presentity, where we
+     * expect to get presence from elsewhere:
+     */
+
+    void set_presence (const std::string presence_);
+
+    void set_status (const std::string status_);
+
+    // method to rename a group for this presentity
+    void rename_group (const std::string old_name,
+		       const std::string new_name);
+
+    /* Those allow the parent Opal::Heap to manage this presentity
+     * effectively.
+     *
+     * First, the 'trigger_saving' signal is to let the heap know that
+     * the live XML document was modified and hence should be saved.
+     *
+     * The 'remove' method asks the presentity to remove all its data
+     * from the live XML document, emit 'trigger_saving' so those
+     * changes get written in the XML source document, and finally
+     * emit 'removed' so the views (and the heap) forget about it as a
+     * presentity.
+     * 
+     */
+    boost::signals2::signal<void(void)> trigger_saving;
+    void remove ();
+
+  private:
+
+    /* this pair of method is to let the user edit the presentity with
+     * a nice form
+     */
+    void edit_presentity ();
+    void edit_presentity_form_submitted (bool submitted,
+					 Ekiga::Form& result);
+
+    // FIXME: this is stupid! We only need the cluster to get a list of
+    // existing groups in the edit presentity form!
+    boost::weak_ptr<Opal::Cluster> cluster;
+    boost::weak_ptr<Ekiga::PresenceCore> presence_core;
+    boost::shared_ptr<xmlDoc> doc;
+    xmlNodePtr node;
+
+    std::string presence;
+    std::string status;
+  };
+
+  typedef boost::shared_ptr<Presentity> PresentityPtr;
+
+};
+
+#endif
