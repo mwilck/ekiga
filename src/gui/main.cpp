mwilck/ekiga

commit e5d5d05b65933ed1e7f114d3ebfa57b5b58de9df

Move gettext init before the first _() call, so that those strings appear translated in the About dialog.

src/gui/main.cpp
@@ -105,6 +105,13 @@ main (int argc,
   signal (SIGPIPE, SIG_IGN);
 #endif
 
+  /* Gettext initialization */
+  path = g_build_filename (DATA_DIR, "locale", NULL);
+  textdomain (GETTEXT_PACKAGE);
+  bindtextdomain (GETTEXT_PACKAGE, path);
+  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+  g_free (path);
+
   /* Application name */
   g_set_application_name (_("Ekiga Softphone"));
 #ifndef WIN32
@@ -120,13 +127,6 @@ main (int argc,
 
   /* Configuration backend initialization */
   gm_conf_init ();
-
-  /* Gettext initialization */
-  path = g_build_filename (DATA_DIR, "locale", NULL);
-  textdomain (GETTEXT_PACKAGE);
-  bindtextdomain (GETTEXT_PACKAGE, path);
-  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-  g_free (path);
 
   /* Arguments initialization */
   GOptionEntry arguments [] =

commit 44009ee6c5990292dbb9cd30306d95f84df38f86

[windows] Fix Ekiga shows only C locale
Bug introduced by commit https://git.gnome.org/browse/ekiga/commit/?id=e5d5d05

src/gui/main.cpp
@@ -105,6 +105,13 @@ main (int argc,
   signal (SIGPIPE, SIG_IGN);
 #endif
 
+  /* initialize platform-specific code */
+  gm_platform_init ();
+#ifdef WIN32
+  // plugins (i.e. the audio/video ptlib/opal codecs) are searched in ./plugins
+  chdir (win32_datadir ());
+#endif
+
   /* Gettext initialization */
   path = g_build_filename (DATA_DIR, "locale", NULL);
   textdomain (GETTEXT_PACKAGE);
@@ -116,13 +123,6 @@ main (int argc,
   g_set_application_name (_("Ekiga Softphone"));
 #ifndef WIN32
   setenv ("PULSE_PROP_application.name", _("Ekiga Softphone"), true);
-#endif
-
-  /* initialize platform-specific code */
-  gm_platform_init ();
-#ifdef WIN32
-  // plugins (i.e. the audio/video ptlib/opal codecs) are searched in ./plugins
-  chdir (win32_datadir ());
 #endif
 
   /* Arguments initialization */