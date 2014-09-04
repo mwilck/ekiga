79555114ba6c7dcf1341179666a40a894c395abd
Eugen Dedu <eugen.dedu@univ-fcomte.fr>
2014-09-04 23:16:17 +0200

    [windows] Remove obsolete file
    Plugins are detected without this file.

--- win32/plugin-gconst.cpp
+++
@@ -1,7 +0,0 @@
-#define PPLUGIN_STATIC_LOAD(serviceName, serviceType) \
-  class PPlugin_##serviceType##_##serviceName##_Registration; \
-  extern PPlugin_##serviceType##_##serviceName##_Registration PPlugin_##serviceType##_##serviceName##_Registration_Instance; \
-  PPlugin_##serviceType##_##serviceName##_Registration const *PPlugin_##serviceType##_##serviceName##_Registration_Static_Library_Loader = &PPlugin_##serviceType##_##serviceName##_Registration_Instance;
-
-//PPLUGIN_STATIC_LOAD(WindowsMultimedia, PSoundChannel);
-//PPLUGIN_STATIC_LOAD(DirectShow, PVideoInputDevice);
